8bf14dac87468ddc325c6bf7c9e62cbc1d40e037
Pierre Jaury <pierre@jaury.eu>
2016-02-21 15:39:15 +0100

    Enable some sieve, spam and quota related plugins for roundcube

--- config/roundcube.inc.php
+++ config/roundcube.inc.php
@@ -2,16 +2,24 @@
 
 $config = array();
 
+// Generals
 $config['db_dsnw'] = 'sqlite:////data/webmail/roundcube.db';
+$config['des_key'] = 'rcmail-!24ByteDESkey*Str';
+$config['identities_level'] = 3;
+$config['reply_all_mode'] = 1;
+
+// Mail servers
 $config['default_host'] = 'localhost';
+$config['default_port'] = 143;
 $config['smtp_server'] = 'localhost';
 $config['smtp_port'] = 25;
-$config['des_key'] = 'rcmail-!24ByteDESkey*Str';
 
 // List of active plugins (in plugins/ directory)
 $config['plugins'] = array(
     'archive',
     'zipdownload',
+    'managesieve',
+    'markasjunk'
 );
 
 // skin name: folder from skins/

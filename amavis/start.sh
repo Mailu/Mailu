f51180415c7409ad5c52d84123e4ed2fe34c2943
Pierre Jaury <pierre@jaury.eu>
2016-03-02 22:31:44 +0100

    Put clamav and amavis togther, fix spam filtering

--- amavis/start.sh
+++ amavis/start.sh
@@ -3,6 +3,10 @@
 # Prepare the databases
 sa-update
 
+# Update the AV database
+freshclam
+
 # Actually run Amavis
+/usr/sbin/clamd
 /usr/sbin/amavisd
 rsyslogd -n
--- clamav/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-freshclam
-clamd

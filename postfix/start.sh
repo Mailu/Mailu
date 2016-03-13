09d737ce9afa08ea1012826c6221537fa8afbd2b
Pierre Jaury <pierre@jaury.eu>
2016-03-13 17:45:39 +0100

    Change the sed delimiter to avoid some errors

--- postfix/start.sh
+++ postfix/start.sh
@@ -2,7 +2,7 @@
 
 # Substitute configuration
 for VARIABLE in `env | cut -f1 -d=`; do
-  sed -i "s,{{ $VARIABLE }},${!VARIABLE},g" /etc/postfix/*.cf
+  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/postfix/*.cf
 done
 
 # Actually run Postfix

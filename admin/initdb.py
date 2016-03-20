54fb9cf60ae243f9142c34bcc1c01877e0334ef1
Pierre Jaury <pierre@jaury.eu>
2016-03-20 11:41:53 +0100

    Add a global admin account when initializing the database

--- admin/initdb.py
+++ admin/initdb.py
@@ -1,6 +1,18 @@
-from freeposte import db
+from freeposte import db, models
+from passlib import hash
 
 
 # Initialize the database
 db.create_all()
+
+domain = models.Domain(name="example.com")
+user = models.User(
+    localpart="admin",
+    domain=domain,
+    global_admin=True,
+    password=hash.sha512_crypt.encrypt("admin").
+)
+
+db.session.add(domain)
+db.session.add(user)
 db.session.commit()

b0a1848bb79ae8ff3c4a0b3e50fb39bb5660f2e9
Pierre Jaury <pierre@jaury.eu>
2016-05-29 15:55:36 +0200

    Substitute variables properly in dovecot configuration

--- dovecot/start.sh
+++ dovecot/start.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+# Substitute configuration
+for VARIABLE in `env | cut -f1 -d=`; do
+  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/dovecot/*.conf
+done
 
 # Fix permissions
 chown -R mail:mail /mail
-chown -R mail:mail /var/lib/dovecot 
+chown -R mail:mail /var/lib/dovecot
 
 # Run dovecot
 exec /usr/sbin/dovecot -c /etc/dovecot/dovecot.conf -F

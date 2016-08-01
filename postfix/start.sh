95492f41bd66ec9669fa43c211ff6818a66d8484
Akuario <jkarlos.b@gmail.com>
2016-08-01 10:29:15 +0200

    Added config folder for Postfix

--- postfix/start.sh
+++ postfix/start.sh
@@ -5,6 +5,25 @@ for VARIABLE in `env | cut -f1 -d=`; do
   sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/postfix/*.cf
 done
 
+# Override Postfix configuration
+if [ -f /config/main.cf ]; then
+  while read line; do
+    postconf -e "$line"
+  done < /config/main.cf
+  echo "Loaded 'config/main.cf'"
+else
+  echo "No extra postfix settings loaded because optional '/config/main.cf' not provided."
+fi
+
+# Include table-map files
+if [ "$(ls -A /config/*.map)" ]; then
+  cp /config/*.map /etc/postfix/
+  postmap /etc/postfix/*.map
+  rm /etc/postfix/*.map
+  chown root:root /etc/postfix/*.db
+  chmod 0600 /etc/postfix/*.db
+fi
+
 # Actually run Postfix
 rm -f /var/run/rsyslogd.pid
 /usr/lib/postfix/master &

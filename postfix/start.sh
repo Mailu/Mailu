efff62c4f80d363c31e12154d4a6de970211f8b5
Akuario <jkarlos.b@gmail.com>
2016-08-02 03:16:16 +0200

    Changed config volume to overrides volume

--- postfix/start.sh
+++ postfix/start.sh
@@ -6,25 +6,25 @@ for VARIABLE in `env | cut -f1 -d=`; do
 done
 
 # Override Postfix configuration
-if [ -f /config/main.cf ]; then
+if [ -f /overrides/postfix.cf ]; then
   while read line; do
     postconf -e "$line"
-  done < /config/main.cf
-  echo "Loaded 'config/main.cf'"
+  done < /overrides/postfix.cf
+  echo "Loaded '/overrides/postfix.cf'"
 else
-  echo "No extra postfix settings loaded because optional '/config/main.cf' not provided."
+  echo "No extra postfix settings loaded because optional '/overrides/postfix.cf' not provided."
 fi
 
 # Include table-map files
-if ls -A /config/*.map 1> /dev/null 2>&1; then
-  cp /config/*.map /etc/postfix/
+if ls -A /overrides/*.map 1> /dev/null 2>&1; then
+  cp /overrides/*.map /etc/postfix/
   postmap /etc/postfix/*.map
   rm /etc/postfix/*.map
   chown root:root /etc/postfix/*.db
   chmod 0600 /etc/postfix/*.db
   echo "Loaded 'map files'"
 else
-  echo "No extra map files loaded because optional '/config/*.map' not provided."
+  echo "No extra map files loaded because optional '/overrides/*.map' not provided."
 fi
 
 # Actually run Postfix

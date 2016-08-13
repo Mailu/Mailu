689c022a4a09a14a0acbc242c8774aaea7899802
Pierre Jaury <pierre@jaury.eu>
2016-08-13 20:01:40 +0200

    Get back to a single forward destination

--- admin/freeposte/admin/models.py
+++ admin/freeposte/admin/models.py
@@ -135,7 +135,7 @@ class User(Email):
 
     # Filters
     forward_enabled = db.Column(db.Boolean(), nullable=False, default=False)
-    forward_destination = db.Column(CommaSeparatedList, nullable=True, default=[])
+    forward_destination = db.Column(db.String(255), nullable=True, default=None)
     reply_enabled = db.Column(db.Boolean(), nullable=False, default=False)
     reply_subject = db.Column(db.String(255), nullable=True, default=None)
     reply_body = db.Column(db.Text(), nullable=True, default=None)

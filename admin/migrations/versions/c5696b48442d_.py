0371da6307254fa021cfc79cd4f1c1a859b91f12
Pierre Jaury <pierre@jaury.eu>
2016-08-15 22:35:25 +0200

    Add the migration script for wildcard aliases, related to #38

---
+++ admin/migrations/versions/c5696b48442d_.py
@@ -0,0 +1,22 @@
+""" Add wildcard aliases
+
+Revision ID: c5696b48442d
+Revises: ff0417f4318f
+Create Date: 2016-08-15 22:19:50.128960
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'c5696b48442d'
+down_revision = 'ff0417f4318f'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('alias', sa.Column('wildcard', sa.Boolean(), nullable=True))
+
+
+def downgrade():
+    op.drop_column('alias', 'wildcard')

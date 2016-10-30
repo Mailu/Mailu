ca930573863054be9e5dc9aa77f7cd7743c6e77d
Pierre Jaury <pierre@jaury.eu>
2016-10-30 13:57:03 +0100

    Make sure that rainloop directories are ready before configuring

--- rainloop/start.sh
+++ rainloop/start.sh
@@ -6,8 +6,9 @@ chown -R www-data:www-data /data
 # There is no cleaner way to setup the default SMTP/IMAP server or to
 # override the configuration
 rm -f /data/_data_/_default_/domains/*
+mkdir -p /data/_data_/_default_/domains/ /data/_data_/_default_/domains/
 cp /default.ini /data/_data_/_default_/domains/
-cp /config.ini /data/_data_/_default_/configs/
+cp /config.ini /data/_data_/_default_/domains/
 
 # Run apache
 exec apache2-foreground

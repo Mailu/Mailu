9156a6a750014552f21e1a84a53ab96aad4e8057
Pierre Jaury <pierre@jaury.eu>
2016-11-06 10:32:13 +0100

    Copy config files properly when rainloop starts

--- rainloop/start.sh
+++ rainloop/start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Fix some permissions
-chown -R www-data:www-data /data
-
 # There is no cleaner way to setup the default SMTP/IMAP server or to
 # override the configuration
 rm -f /data/_data_/_default_/domains/*
-mkdir -p /data/_data_/_default_/domains/ /data/_data_/_default_/domains/
+mkdir -p /data/_data_/_default_/domains/ /data/_data_/_default_/configs/
 cp /default.ini /data/_data_/_default_/domains/
-cp /config.ini /data/_data_/_default_/domains/
+cp /config.ini /data/_data_/_default_/configs/
+
+# Fix some permissions
+chown -R www-data:www-data /data
 
 # Run apache
 exec apache2-foreground

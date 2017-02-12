be5562620e9b57fa77b384ba751060f83c6b1237
Pierre Jaury <pierre@jaury.eu>
2017-02-12 16:32:46 +0100

    Enable https and --preload on the admin to improve letsencrypt support

--- nginx/Dockerfile
+++ nginx/Dockerfile
@@ -1,8 +1,8 @@
 FROM nginx:alpine
 
-RUN apk add --update nginx-lua && rm -rf /var/cache/apk/*
+RUN apk add --update nginx-lua openssl && rm -rf /var/cache/apk/*
 
-COPY nginx.conf /etc/nginx/nginx.conf
+COPY nginx.conf.default /etc/nginx/nginx.conf.default
 COPY nginx.conf.fallback /etc/nginx/nginx.conf.fallback
 
 COPY start.sh /start.sh
--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-if [[ ! -z ENABLE_CERTBOT && ! -f /certs/cert.pem ]]; then
+if [[ -z ENABLE_CERTBOT || -f /certs/cert.pem ]]
+then
+  cp /etc/nginx/nginx.conf.default /etc/nginx/nginx.conf
+else
+  openssl req -newkey rsa:2048 -x509 -keyout /tmp/snakeoil.pem -out /tmp/snakeoil.pem -days 365 -nodes -subj "/C=NA/ST=None/
+L=None/O=None/CN=$DOMAIN"
   cp /etc/nginx/nginx.conf.fallback /etc/nginx/nginx.conf
 fi
 

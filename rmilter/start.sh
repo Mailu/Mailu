117e858df3148e27947b662ff633290979b42aa1
Fabian Letzkus <mail@donotconnect.de>
2017-06-25 16:15:47 +0000

    Added parameter checking in rmilter image, added parameter in docker-compose, added parameter in .env

--- rmilter/start.sh
+++ rmilter/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 rm -f /var/run/rsyslogd.pid
+if [ "$ANTIVIRUS" == "clamav" ];
+then
+	echo "# .try_include /etc/rmilter-clamav.conf" >>  /etc/rmilter.conf
+fi
 rmilter -c /etc/rmilter.conf
 rsyslogd -n

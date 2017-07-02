4053273fe6d50551ad022ac0f019bea5ae01ad11
kaiyou <pierre@jaury.eu>
2017-07-02 16:26:31 +0200

    Use relaynets in rmilter whitelist, fixes #140

--- rmilter/start.sh
+++ rmilter/start.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+export WHITELIST=$(echo "$RELAYNETS" | sed 's/ /,/g')
+
+# Substitute configuration
+for VARIABLE in `env | cut -f1 -d=`; do
+  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/rmilter.conf
+done
 
 rm -f /var/run/rsyslogd.pid
 rmilter -c /etc/rmilter.conf

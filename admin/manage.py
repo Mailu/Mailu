e4b338c9a4c51dda106e558ba03c4ebd6131ec8c
Dmytro Makovey <dmakovey@stanford.edu>
2017-08-25 12:44:05 -0700

    add config sync for bulk operations on users and aliases driven by config management systems etc.

--- admin/manage.py
+++ admin/manage.py
@@ -72,6 +72,54 @@ def user_import(localpart, domain_name, password_hash, hash_scheme='SHA512-CRYPT
     db.session.add(user)
     db.session.commit()
 
+@manager.command
+def config_update():
+    """sync configuration with data from stdin"""
+    import yaml, sys
+    new_config=yaml.load(sys.stdin)
+    # print new_config
+    users=new_config['users']
+    for user_config in users:
+        localpart=user_config['localpart']
+        domain_name=user_config['domain']
+        password_hash=user_config['password_hash']
+        hash_scheme=user_config['hash_scheme']
+        domain = models.Domain.query.get(domain_name)
+        if not domain:
+            domain = models.Domain(name=domain_name)
+            db.session.add(domain)
+        user = models.User.query.get('{0}@{1}'.format(localpart,domain_name))
+        if not user:
+            user = models.User(
+                localpart=localpart,
+                domain=domain,
+                global_admin=False
+            )
+        user.set_password(password_hash, hash_scheme=hash_scheme, raw=True)
+        db.session.add(user)
+
+    aliases=new_config['aliases']
+    for alias_config in aliases:
+        localpart=alias_config['localpart']
+        domain_name=alias_config['domain']
+        destination=alias_config['destination']
+        domain = models.Domain.query.get(domain_name)
+        if not domain:
+            domain = models.Domain(name=domain_name)
+            db.session.add(domain)
+        alias = models.Alias.query.get('{0}@{1}'.format(localpart, domain_name))
+        if not alias:
+            alias = models.Alias(
+                localpart=localpart,
+                domain=domain,
+                destination=destination.split(','),
+                email="%s@%s" % (localpart, domain_name)
+            )
+        else:
+            alias.destination = destination.split(',')
+        db.session.add(alias)
+    
+    db.session.commit()
 
 @manager.command
 def alias(localpart, domain_name, destination):

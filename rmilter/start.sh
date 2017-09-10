a092e3f9b23f27f623d3e1515654c1ef89589a3d
kaiyou <pierre@jaury.eu>
2017-09-10 19:42:54 +0200

    Properly include the rmilter clamav configuration

--- rmilter/start.sh
+++ rmilter/start.sh
@@ -10,7 +10,7 @@ done
 rm -f /var/run/rsyslogd.pid
 if [ "$ANTIVIRUS" == "clamav" ];
 then
-	echo "# .try_include /etc/rmilter-clamav.conf" >>  /etc/rmilter.conf
+	echo ".try_include /etc/rmilter-clamav.conf" >>  /etc/rmilter.conf
 fi
 rmilter -c /etc/rmilter.conf
 rsyslogd -n

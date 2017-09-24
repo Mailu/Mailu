5097f578551ce7f1c5dfccc4b4fc63621ff9f990
kaiyou <pierre@jaury.eu>
2017-09-24 15:43:46 +0200

    Redirect nginx authentication requests to the admin container

--- nginx/start.py
+++ nginx/start.py
@@ -7,6 +7,7 @@ import socket
 convert = lambda src, dst: open(dst, "w").write(jinja2.Template(open(src).read()).render(**os.environ))
 
 # Actual startup script
-os.environ["ADMIN_ADDRESS"] = socket.gethostbyname("admin")
+if "ADMIN_ADDRESS" not in os.environ:
+    os.environ["ADMIN_ADDRESS"] = socket.gethostbyname("admin")
 convert("/conf/nginx.conf", "/etc/nginx/nginx.conf")
 os.execv("/usr/sbin/nginx", ["nginx", "-g", "daemon off;"])

baff8dd0432d7da4c9a719e9cb66575d132e541d
kaiyou <pierre@jaury.eu>
2017-11-05 18:12:26 +0100

    Avoid blacklisting the webmail

--- core/admin/mailu/internal/__init__.py
+++ core/admin/mailu/internal/__init__.py
@@ -1,6 +1,19 @@
-from flask import Blueprint
+from mailu import limiter
+
+import socket
+import flask
 
 
-internal = Blueprint('internal', __name__)
+internal = flask.Blueprint('internal', __name__)
+
+
+@limiter.request_filter
+def whitelist_webmail():
+    try:
+        return flask.request.headers["Client-Ip"] ==\
+            socket.gethostbyname("webmail")
+    except:
+        return False
+
 
 from mailu.internal import views

eb328719042a4c86d550b43ad2d548b5b9312c8b
kaiyou <pierre@jaury.eu>
2017-11-13 21:38:04 +0100

    Force nginx to run dns queries at runtime

--- core/nginx/config.py
+++ core/nginx/config.py
@@ -7,6 +7,13 @@ convert = lambda src, dst, args: open(dst, "w").write(jinja2.Template(open(src).
 
 args = os.environ.copy()
 
+# Get the first DNS server
+with open("/etc/resolv.conf") as handle:
+    content = handle.read().split()
+    args["RESOLVER"] = content[content.index("nameserver") + 1]
+
+
+# TLS configuration
 args["TLS"] = {
     "cert": ("/certs/cert.pem", "/certs/key.pem"),
     "mail": ("/certs/cert.pem", "/certs/key.pem"),
@@ -20,6 +27,7 @@ if args["TLS"] and not all(os.path.exists(file_path) for file_path in args["TLS"
     args["TLS_ERROR"] = "yes"
 
 
+# Build final configuration paths
 convert("/conf/tls.conf", "/etc/nginx/tls.conf", args)
 convert("/conf/nginx.conf", "/etc/nginx/nginx.conf", args)
 os.system("nginx -s reload")

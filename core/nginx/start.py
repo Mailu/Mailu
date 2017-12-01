5bb3ea23edca170125aebbfd4dba55e89e108b22
Greg Fitzgerald <greg@gregf.org>
2017-11-30 19:38:26 -0500

    Use a predefined dhparam.pem, This fixes issue #322

--- core/nginx/start.py
+++ core/nginx/start.py
@@ -4,10 +4,6 @@ import os
 import subprocess
 
 
-# Actual startup script
-if not os.path.exists("/certs/dhparam.pem") and os.environ["TLS_FLAVOR"] != "notls":
-    os.system("openssl dhparam -out /certs/dhparam.pem 4096")
-
 if os.environ["TLS_FLAVOR"] == "letsencrypt":
     subprocess.Popen(["/letsencrypt.py"])
 

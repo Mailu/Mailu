b11ebbda4d423b7c877b3192e5c06379b7c022d9
AdrienM <amaurel90@gmail.com>
2018-03-24 15:12:30 +0100

    Dovecot working mysql

--- core/dovecot/start.py
+++ core/dovecot/start.py
@@ -5,6 +5,12 @@ import os
 import socket
 import glob
 
+if os.environ["DB_TYPE"] == "mysql":
+	if "HOST_DB" not in os.environ:
+		os.environ["HOST_DB"] = "database:3306"
+else:
+	os.environ["DB_TYPE"] = "sqlite"
+
 convert = lambda src, dst: open(dst, "w").write(jinja2.Template(open(src).read()).render(**os.environ))
 
 # Actual startup script
@@ -12,10 +18,10 @@ os.environ["FRONT_ADDRESS"] = socket.gethostbyname("front")
 if os.environ["WEBMAIL"] != "none":
 	os.environ["WEBMAIL_ADDRESS"] = socket.gethostbyname("webmail")
 
-for dovecot_file in glob.glob("/conf/*"):
+for dovecot_file in glob.glob("/conf/*.conf"):
     convert(dovecot_file, os.path.join("/etc/dovecot", os.path.basename(dovecot_file)))
 
-for maps_file in glob.glob("/conf/" + os.environ["DB_TYPE"] + "/*.cf"):
+for maps_file in glob.glob("/conf/" + os.environ["DB_TYPE"] + "/*"):
 	convert(maps_file, os.path.join("/etc/dovecot", os.path.basename(maps_file)))
 
 # Run dovecot

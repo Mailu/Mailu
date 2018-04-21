186c30d2ac50eea4c093b0a15152ebedd605bdd8
kaiyou <pierre@jaury.eu>
2018-04-21 12:55:14 +0200

    Have the admin listen on ipv6

--- core/admin/start.sh
+++ core/admin/start.sh
@@ -2,4 +2,4 @@
 
 python manage.py advertise
 python manage.py db upgrade
-gunicorn -w 4 -b 0.0.0.0:80 --access-logfile - --error-logfile - --preload mailu:app
+gunicorn -w 4 -b 0.0.0.0:80 -b [::]:80 --access-logfile - --error-logfile - --preload mailu:app

9de58279a526cba01d810a0221e9e78b47e9714d
ofthesun9 <olivier@ofthesun.net>
2018-06-02 07:04:14 +0200

    Update start.py
    replace /var/log/nginx.pid with /var/run/nginx.pid to ensure consistency with core/nginx/conf/nginx.conf

--- core/nginx/start.py
+++ core/nginx/start.py
@@ -4,8 +4,8 @@ import os
 import subprocess
 
 # Check if a stale pid file exists
-if os.path.exists("/var/log/nginx.pid"):
-    os.remove("/var/log/nginx.pid")
+if os.path.exists("/var/run/nginx.pid"):
+    os.remove("/var/run/nginx.pid")
 
 if os.environ["TLS_FLAVOR"] in [ "letsencrypt","mail-letsencrypt" ]:
     subprocess.Popen(["/letsencrypt.py"])

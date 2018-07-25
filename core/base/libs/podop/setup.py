eb6b1866f15bb2745b5fb36138c4a1e656ebaf7f
Pierre Jaury <pierre@jaury.eu>
2018-07-25 20:49:16 +0200

    Specify dependencies in the setup script

--- core/base/libs/podop/setup.py
+++ core/base/libs/podop/setup.py
@@ -2,13 +2,21 @@
 
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="Podop",
-    version="0.1",
+    name="podop",
+    version="0.1.1",
     description="Postfix and Dovecot proxy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Pierre Jaury",
     author_email="pierre@jaury.eu",
     url="https://github.com/mailu/podop.git",
     packages=["podop"],
-    scripts=["scripts/podop"]
+    scripts=["scripts/podop"],
+    install_requires=[
+        "aiohttp"
+    ]
 )

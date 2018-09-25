650575d54baaff7ba53909a9c9d9ebaa919244a6
Thomas Sänger <thomas@gecko.space>
2018-09-25 21:46:07 +0200

    admin: number of workers == number of CPU-cores

--- core/admin/start.sh
+++ core/admin/start.sh
@@ -2,4 +2,4 @@
 
 python manage.py advertise
 python manage.py db upgrade
-gunicorn -w 4 -b :80 --access-logfile - --error-logfile - --preload mailu:app
+gunicorn -w $(nproc) -b :80 --access-logfile - --error-logfile - --preload mailu:app

d2b98ae323e7695e61c409c831b50b01540de945
kaiyou <pierre@jaury.eu>
2018-09-26 17:45:27 +0200

    Update to 0.2.2

--- core/base/libs/podop/setup.py
+++ core/base/libs/podop/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="podop",
-    version="0.2.1",
+    version="0.2.2",
     description="Postfix and Dovecot proxy",
     long_description=long_description,
     long_description_content_type="text/markdown",

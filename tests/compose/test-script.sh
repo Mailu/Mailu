027b8f36dfb5a2a8b0a479324af7a00294a85e11
Ionut Filip <ionut.filip@usrpro.com>
2018-10-04 17:37:09 +0300

    Added a check for container status

--- tests/compose/test-script.sh
+++ tests/compose/test-script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 containers_check() {
 	for container in mailu_webmail_1 mailu_imap_1 mailu_smtp_1 mailu_antispam_1 mailu_admin_1 mailu_redis_1 mailu_antivirus_1 mailu_webdav_1 mailu_fetchmail_1 mailu_front_1; do
-	        docker inspect $container | grep '"Status": "running"' || exit 1
+		echo "Checking $container"
+		docker inspect $container | grep '"Status": "running"' || exit 1
 	done
 }
 

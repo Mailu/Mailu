bc70be35c22007aadf72d3f6c3ddca71e2665993
Ionut Filip <ionut.filip@usrpro.com>
2018-10-04 17:18:48 +0300

    Added function to check docker containers

--- tests/compose/test-script.sh
+++ tests/compose/test-script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+containers_check() {
+	for container in mailu_webmail_1 mailu_imap_1 mailu_smtp_1 mailu_antispam_1 mailu_admin_1 mailu_redis_1 mailu_antivirus_1 mailu_webdav_1 mailu_fetchmail_1 mailu_front_1; do
+	        docker inspect $container | grep '"Status": "running"' || exit 1
+	done
+}
+
 for file in tests/compose/*.env ; do
 	cp $file .env
 	docker-compose -f tests/compose/run.yml up -d
+	sleep 1m
+	containers_check
 done
-sleep 2m
+

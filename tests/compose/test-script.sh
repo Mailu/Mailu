71eed72e212689e28a4dd366aba4df5b15219fe1
Ionut Filip <ionut.filip@usrpro.com>
2018-10-04 17:51:17 +0300

    Changed exit condition

--- tests/compose/test-script.sh
+++ tests/compose/test-script.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 containers_check() {
+	STATUS=0
 	for container in mailu_webmail_1 mailu_imap_1 mailu_smtp_1 mailu_antispam_1 mailu_admin_1 mailu_redis_1 mailu_antivirus_1 mailu_webdav_1 mailu_fetchmail_1 mailu_front_1; do
 		echo "Checking $container"
-		docker inspect $container | grep '"Status": "running"' || exit 1
+		docker inspect $container | grep '"Status": "running"' || STATUS=1
 	done
+	return $STATUS
 }
 
 for file in tests/compose/*.env ; do
 	cp $file .env
 	docker-compose -f tests/compose/run.yml up -d
 	sleep 1m
-	containers_check
+	containers_check || exit 1
 done
 

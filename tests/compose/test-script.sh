0f8d176e9fb50261e18b7781f7056b3168294de9
Tim Möhlmann <muhlemmer@gmail.com>
2018-10-11 13:46:03 +0300

    Some improvements:  - Print some helpfull messages to find back the test stages in travis logs  - Fail the script when `docker-compose` fails  - Include fetchmail test  - Use `docker-compose logs` for better log tagging  - Increased the wait time

--- tests/compose/test-script.sh
+++ tests/compose/test-script.sh
@@ -8,19 +8,30 @@ containers=(
 	redis
 	antivirus
 	webdav
-#	fetchmail
+	fetchmail
 	front
 )
 
+#Colors (bold)
+YEL='\e[1;33m'
+RED='\e[1;31m'
+RES='\e[0m'
+
+# Compose file to use
+DC_FILE="tests/compose/run.yml"
+
 # Time to sleep in minutes after starting the containers
-WAIT=1
+WAIT=5
 
 containers_check() {
 	status=0
 	for container in "${containers[@]}"; do
 		name="${DOCKER_ORG}_${container}_1"
-		echo "Checking $name"
-		docker inspect "$name" | grep '"Status": "running"' || status=1
+		echo -e "\n${YEL}Checking ${name}${RES}"
+		if ! docker inspect "$name" | grep '"Status": "running"'; then
+			echo -e "${RED}Fail!${RES}"
+			status=1
+		fi
 	done
 	docker ps -a
 	return $status
@@ -28,14 +39,13 @@ containers_check() {
 
 container_logs() {
 	for container in "${containers[@]}"; do
-		name="${DOCKER_ORG}_${container}_1"
-                echo "Showing logs for $name"
-                docker container logs "$name"
+                echo -e "\n${YEL}Showing logs for ${container}${RES}"
+                docker-compose -f $DC_FILE -p $DOCKER_ORG logs -t $container
         done
 }
 
 clean() {
-	docker-compose -f tests/compose/run.yml -p $DOCKER_ORG down || exit 1
+	docker-compose -f $DC_FILE -p $DOCKER_ORG down || exit 1
 	rm -fv .env
 }
 
@@ -46,10 +56,15 @@ die() {
 }
 
 for file in tests/compose/*.env ; do
-	cp $file .env
-	docker-compose -f tests/compose/run.yml -p $DOCKER_ORG up -d
-	echo -e "\nSleeping for ${WAIT} minutes" # Clean terminal distortion from docker-compose in travis
-	travis_wait sleep ${WAIT}m || sleep ${WAIT}m #Fallback sleep for local run
+	echo -e "\n${YEL}Starting test for ${file}${RES}"
+	cp -v $file .env
+	docker-compose -f $DC_FILE -p $DOCKER_ORG up -d || die 1
+
+	# Clean terminal distortion from docker-compose in travis
+	echo -e "\n${YEL}Sleeping for ${WAIT} minutes${RES}\n"
+
+	travis_wait sleep ${WAIT}m || sleep ${WAIT}s # Fallback sleep for local run
+
 	container_logs
 	containers_check || die 1
 	clean

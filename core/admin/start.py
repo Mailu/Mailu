42e2dbe35d97dbdea05d60bb8443058d5c7d6b9d
Tim Möhlmann <muhlemmer@gmail.com>
2018-10-31 19:17:23 +0200

    Standarize image by using shared / similair layers

--- core/admin/start.py
+++ core/admin/start.py
@@ -1,7 +1,7 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import os
 
-os.system("python manage.py advertise")
-os.system("python manage.py db upgrade")
+os.system("python3 manage.py advertise")
+os.system("python3 manage.py db upgrade")
 os.system("gunicorn -w 4 -b :80 --access-logfile - --error-logfile - --preload mailu:app")
--- services/rspamd/start.py
+++ services/rspamd/start.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import jinja2
 import os

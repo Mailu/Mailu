e668d360e80580dd508fe982f2d326fb9c91de70
Ionut Filip <ionut.philip@gmail.com>
2018-11-23 14:37:55 +0200

    Changed user creation command

--- tests/compose/core/00_create_users.sh
+++ tests/compose/core/00_create_users.sh
@@ -1,4 +1,4 @@
 echo "Creating users ..."
 docker-compose -f tests/compose/core/docker-compose.yml exec admin flask mailu admin admin mailu.io password || exit 1
-docker-compose -f tests/compose/core/docker-compose.yml exec admin flask mailu user --hash_scheme='SHA512-CRYPT' user mailu.io 'password' || exit 1
+docker-compose -f tests/compose/core/docker-compose.yml exec admin flask mailu user user mailu.io 'password' 'SHA512-CRYPT' || exit 1
 echo "Admin and user successfully created!"

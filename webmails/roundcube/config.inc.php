66e4c9bb2835526610b91120813cedf0553663f8
guruevi <vanooste@rcbi.rochester.edu>
2019-01-06 16:34:09 -0500

    Allow for multiple identities
    Mailu is already set up to disallow non-existing identities at the SMTP level. People can already set up identities on external email clients. This should allow users to set up multiple identities within Roundcube

--- webmails/roundcube/config.inc.php
+++ webmails/roundcube/config.inc.php
@@ -7,7 +7,7 @@ $config['db_dsnw'] = 'sqlite:////data/roundcube.db';
 $config['temp_dir'] = '/tmp/';
 $config['des_key'] = getenv('SECRET_KEY');
 $config['cipher_method'] = 'AES-256-CBC';
-$config['identities_level'] = 3;
+$config['identities_level'] = 0;
 $config['reply_all_mode'] = 1;
 
 // List of active plugins (in plugins/ directory)

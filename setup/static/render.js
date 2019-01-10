37a07e329725f066ef4636a1805680b68595cc9f
Ionut Filip <ionut.philip@gmail.com>
2019-01-10 15:44:02 +0200

    Update setup utility with mysql db flavor

--- setup/static/render.js
+++ setup/static/render.js
@@ -36,8 +36,10 @@ $(document).ready(function() {
 $(document).ready(function() {
 	if ($("#database").val() == 'sqlite') {
 		$("#postgres_db").hide();
-	} else {
+	} else if ($("#database").val() == 'postgresql') {
 		$("#postgres_db").show();
+	} else if ($("#database").val() == 'mysql') {
+		$("#external_db").show();
 	}
 	if ($('#external_psql').prop('checked')) {
 		$("#external_db").show();
@@ -45,8 +47,18 @@ $(document).ready(function() {
 	$("#database").click(function() {
 		if (this.value == 'sqlite') {
 			$("#postgres_db").hide();
-		} else {
+			$("#external_db").hide();
+		} else if (this.value == 'postgresql') {
 			$("#postgres_db").show();
+			$("#external_db").hide();
+			$("#external_db").prop('checked', false);
+		} else if (this.value == 'mysql') {
+			$("#postgres_db").hide();
+			$("#external_db").show();
+			$("#db_user").prop('required',true);
+			$("#db_pw").prop('required',true);
+			$("#db_url").prop('required',true);
+			$("#db_name").prop('required',true);
 		}
 	});
 	$("#external_psql").change(function() {

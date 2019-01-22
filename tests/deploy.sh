9d45ed2fd305ebe6214ca73cca7315d3b6a42b39
Tim Möhlmann <muhlemmer@gmail.com>
2019-01-22 16:39:42 +0200

    Automatic creation of review images
    - Enable bors-ng for better merging
    - Re-enable mergify with a fixed up syntax, it will now issue bors commands.
    - Pull request build images are now pushed to docker hub
    
    Deploy in script

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-docker login -u $DOCKER_UN -p $DOCKER_PW
-docker-compose -f tests/build.yml push
+# Re-tag images fir PR testing
+if [ $TRAVIS_PULL_REQUEST != false ]; then
+    export MAILU_VERSION="${TRAVIS_BRANCH}-${TRAVIS_PULL_REQUEST}"
+    export DOCKER_ORG="mailutest"
+    docker-compose -f tests/build.yml build
+fi
+
+if [ -n $DOCKER_UN ] && [ -n $DOCKER_PW ]; then
+    docker login -u $DOCKER_UN -p $DOCKER_PW
+    docker-compose -f tests/build.yml push
+fi

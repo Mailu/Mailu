b1e7c44f0cbf5eaf1b5105e9e914c0dbe6cb9238
Tim Möhlmann <muhlemmer@gmail.com>
2019-01-23 17:10:08 +0200

    Only push when master and 1.6 target branch

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Re-tag images fir PR testing
+# Re-tag images for PR testing
 if [ $TRAVIS_PULL_REQUEST != false ]; then
     export MAILU_VERSION="${TRAVIS_BRANCH}-${TRAVIS_PULL_REQUEST}"
     export DOCKER_ORG="mailutest"
     docker-compose -f tests/build.yml build
 fi
 
-if [ -n $DOCKER_UN ] && [ -n $DOCKER_PW ]; then
+
+# Note that in case of a PR, the branch is the one we are merging into
+if [ -n $DOCKER_UN ] && [ -n $DOCKER_PW ] && \
+{ [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "1.6" ]; }; then
     docker login -u $DOCKER_UN -p $DOCKER_PW
     docker-compose -f tests/build.yml push
 fi

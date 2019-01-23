c5b8c4f28c6c3d9ab1290726c51c9f5c25757a19
Tim Möhlmann <muhlemmer@gmail.com>
2019-01-23 12:49:43 +0200

    Create testing images with travis

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker login -u $DOCKER_UN -p $DOCKER_PW
-docker-compose -f tests/build.yml push
+if [ -n $DOCKER_UN ] && [ -n $DOCKER_PW ]; then
+    docker login -u $DOCKER_UN -p $DOCKER_PW
+    docker-compose -f tests/build.yml push
+fi

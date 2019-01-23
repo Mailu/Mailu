3f4f811f10b5d9e50e270e8e84fe768d79992c14
Tim Möhlmann <muhlemmer@gmail.com>
2019-01-23 13:07:20 +0200

    PR checking in deploy.sh

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Re-tag images fir PR testing
+if [ $TRAVIS_PULL_REQUEST != false ] then;
+    export MAILU_VERSION="${TRAVIS_BRANCH}-${TRAVIS_PULL_REQUEST}"
+    export DOCKER_ORG="mailutest"
+    docker-compose -f tests/build.yml build
+fi
+
 if [ -n $DOCKER_UN ] && [ -n $DOCKER_PW ]; then
     docker login -u $DOCKER_UN -p $DOCKER_PW
     docker-compose -f tests/build.yml push

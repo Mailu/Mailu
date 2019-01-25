a071181c610b19420845cec7e7b8d99c66cf918a
cristi <cristi.posoiu@gmail.com>
2019-01-25 16:04:20 +0200

    Allow to automatically create admin account during startup.

--- core/admin/start.py
+++ core/admin/start.py
@@ -4,4 +4,12 @@ import os
 
 os.system("flask mailu advertise")
 os.system("flask db upgrade")
+
+if 'INITIAL_ADMIN_ACCOUNT' in os.environ and 'INITIAL_ADMIN_DOMAIN' in os.environ and 'INITIAL_ADMIN_PW'  in os.environ:
+    mode = 'ifmissing'
+    if 'INITIAL_ADMIN_MODE' in os.environ:
+        mode = os.environ['INITIAL_ADMIN_MODE']
+    os.system("flask mailu admin %s %s '%s' --mode %s" % (
+        os.environ['INITIAL_ADMIN_ACCOUNT'], os.environ['INITIAL_ADMIN_DOMAIN'], os.environ['INITIAL_ADMIN_PW'], mode))
+
 os.system("gunicorn -w 4 -b :80 --access-logfile - --error-logfile - --preload 'mailu:create_app()'")

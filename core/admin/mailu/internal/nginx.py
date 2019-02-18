0ce0b5ec02b2ff2644cb709975d6d338cddcd728
Ionut Filip <ionut.philip@gmail.com>
2019-02-18 15:41:22 +0200

    Fix addresses in front

--- core/admin/mailu/internal/nginx.py
+++ core/admin/mailu/internal/nginx.py
@@ -80,12 +80,12 @@ def extract_host_port(host_and_port, default_port):
 
 def get_server(protocol, authenticated=False):
     if protocol == "imap":
-        hostname, port = extract_host_port(app.config['HOST_IMAP'], 143)
+        hostname, port = extract_host_port(app.config['IMAP_ADDRESS'], 143)
     elif protocol == "pop3":
-        hostname, port = extract_host_port(app.config['HOST_POP3'], 110)
+        hostname, port = extract_host_port(app.config['POP3_ADDRESS'], 110)
     elif protocol == "smtp":
         if authenticated:
-            hostname, port = extract_host_port(app.config['HOST_AUTHSMTP'], 10025)
+            hostname, port = extract_host_port(app.config['AUTHSMTP_ADDRESS'], 10025)
         else:
-            hostname, port = extract_host_port(app.config['HOST_SMTP'], 25)
+            hostname, port = extract_host_port(app.config['SMTP_ADDRESS'], 25)
     return hostname, port

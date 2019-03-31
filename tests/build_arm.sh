b711ef82da9f154448ea1622d2d1b7aa3f619020
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-03-31 15:52:43 -0400

    use edge for radicale

--- tests/build_arm.sh
+++ tests/build_arm.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
 
-DISTRO="balenalib/rpi-alpine:3.8"
+ALPINE_VER="3.8"
+DISTRO="balenalib/rpi-alpine:$ALPINE_VER"
+EDGE_DISTRO="balenalib/rpi-alpine:edge"
 PHP="arm32v7/php:7.2-apache"
 QEMU="$(which qemu-arm-static)"
 cp $QEMU ../webmails/rainloop/
 cp $QEMU ../webmails/roundcube/
 
-docker-compose -f build.yml build --build-arg DISTRO=$DISTRO --build-arg PHP_DISTRO=$PHP --parallel $@
+docker-compose -f build.yml build --build-arg DISTRO=$DISTRO --build-arg PHP_DISTRO=$PHP --build-arg EDGE_DISTRO=$EDGE_DISTRO --parallel $@

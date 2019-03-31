c0f4126a6c9519039f5a381c04993b80c67ce0a3
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-03-31 09:17:11 -0400

    use 3.8 for arm too

--- tests/build_arm.sh
+++ tests/build_arm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-DISTRO="balenalib/rpi-alpine"
+DISTRO="balenalib/rpi-alpine:3.8"
 PHP="arm32v7/php:7.2-apache"
 QEMU="$(which qemu-arm-static)"
 cp $QEMU ../webmails/rainloop/
 cp $QEMU ../webmails/roundcube/
 
-docker-compose -f build.yml build --build-arg DISTRO=$DISTRO --build-arg PHP_DISTRO=$PHP $@  # --parallel
+docker-compose -f build.yml build --build-arg DISTRO=$DISTRO --build-arg PHP_DISTRO=$PHP --parallel $@

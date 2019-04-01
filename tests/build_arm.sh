a8f55aa73af283dddb68852d099881bebc905a0f
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-04-01 11:31:19 -0400

    copy arm qemu

--- tests/build_arm.sh
+++ tests/build_arm.sh
@@ -7,11 +7,10 @@ EDGE_DISTRO="balenalib/rpi-alpine:edge"
 # Used for webmails
 PHP="arm32v7/php:7.2-apache"
 
-# NOTE: Might not be needed since using multiarch/qemu
-QEMU="$(which qemu-arm-static)"
-cp $QEMU ../webmails/rainloop/
-cp $QEMU ../webmails/roundcube/
-
 # use qemu-*-static from docker container
 docker run --rm --privileged multiarch/qemu-user-static:register 
-docker-compose -f build.yml build --build-arg DISTRO=$DISTRO --build-arg PHP_DISTRO=$PHP --build-arg EDGE_DISTRO=$EDGE_DISTRO --parallel $@
+docker-compose -f build.yml build \
+  --build-arg DISTRO=$DISTRO \
+  --build-arg PHP_DISTRO=$PHP \
+  --build-arg EDGE_DISTRO=$EDGE_DISTRO \
+  --parallel $@

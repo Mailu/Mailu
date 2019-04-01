199b74a1449ff6e5a549e2b5e51ce51bd8c69ec4
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-03-31 23:47:39 -0400

    update comments, add helper script to push local build to rpi

--- tests/build_arm.sh
+++ tests/build_arm.sh
@@ -2,10 +2,16 @@
 
 ALPINE_VER="3.8"
 DISTRO="balenalib/rpi-alpine:$ALPINE_VER"
+# Used for Radicale
 EDGE_DISTRO="balenalib/rpi-alpine:edge"
+# Used for webmails
 PHP="arm32v7/php:7.2-apache"
+
+# NOTE: Might not be needed since using multiarch/qemu
 QEMU="$(which qemu-arm-static)"
 cp $QEMU ../webmails/rainloop/
 cp $QEMU ../webmails/roundcube/
 
+# use qemu-*-static from docker container
+docker run --rm --privileged multiarch/qemu-user-static:register 
 docker-compose -f build.yml build --build-arg DISTRO=$DISTRO --build-arg PHP_DISTRO=$PHP --build-arg EDGE_DISTRO=$EDGE_DISTRO --parallel $@

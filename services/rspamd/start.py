8bf629cb46b50e6d8afcab0c89817e95773727b1
Igor Rzegocki <igor@rzegocki.pl>
2019-05-07 11:42:08 +0200

    Fixed hardcoded antispam and antivirus host addresses
    Fixes #978
    
    (cherry picked from commit 6f973a2e4be79c5f3e6aefd2251af6dfbce52bae)
    
    # Conflicts:
    #	services/rspamd/start.py

--- services/rspamd/start.py
+++ services/rspamd/start.py
@@ -9,9 +9,18 @@ from socrate import system, conf
 log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))
 
 # Actual startup script
+<<<<<<< HEAD
 os.environ["FRONT_ADDRESS"] = system.resolve_address(os.environ.get("FRONT_ADDRESS", "front"))
 
 if "HOST_REDIS" not in os.environ: os.environ["HOST_REDIS"] = "redis"
+=======
+
+os.environ["FRONT_ADDRESS"] = system.resolve_address(os.environ.get("HOST_FRONT", "front"))
+
+if "HOST_REDIS" not in os.environ:
+    os.environ["REDIS_ADDRESS"] = system.resolve_address(os.environ.get("HOST_REDIS", "redis"))
+os.environ["HOST_ANTIVIRUS"] = system.resolve_address(os.environ.get("HOST_ANTIVIRUS", "antivirus:3310"))
+>>>>>>> 6f973a2e... Fixed hardcoded antispam and antivirus host addresses
 
 for rspamd_file in glob.glob("/conf/*"):
     conf.jinja(rspamd_file, os.environ, os.path.join("/etc/rspamd/local.d", os.path.basename(rspamd_file)))

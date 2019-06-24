baa5a8a4e0111d79d02cb2ef9a4f27d6c5ac850f
kaiyou <pierre@jaury.eu>
2019-06-24 23:54:53 +0200

    Fix hostname resolution

--- core/admin/mailu/configuration.py
+++ core/admin/mailu/configuration.py
@@ -81,7 +81,7 @@ class ConfigManager(dict):
 
     def resolve_host(self):
         optional = [item for item in self.OPTIONAL_HOSTS if item in self.config]
-        for item in self.HOSTS + optional:
+        for item in list(self.HOSTS) + optional:
             host = 'HOST_' + item
             self.config[host] = system.resolve_address(self.config[host])
 

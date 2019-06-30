81a8acf9ec4a83327120e91c4f853e9ad3726c05
hoellen <dev@hoellen.eu>
2019-06-30 13:18:55 +0200

    fix resolve issue

--- core/admin/mailu/configuration.py
+++ core/admin/mailu/configuration.py
@@ -80,7 +80,7 @@ class ConfigManager(dict):
         self.config = dict()
 
     def resolve_host(self):
-        optional = [item for item in self.OPTIONAL_HOSTS if item in self.config]
+        optional = [item for item in self.OPTIONAL_HOSTS if item in self.config and self.config[item] != "none"]
         for item in list(self.HOSTS) + optional:
             host = 'HOST_' + item
             self.config[host] = system.resolve_address(self.config[host])

90f678de52a311464553121e3a4670fbdb2bf404
hoellen <dev@hoellen.eu>
2019-07-08 16:53:25 +0200

    add app.css and fix align of menu button

--- core/admin/assets/app.js
+++ core/admin/assets/app.js
@@ -1,8 +1,10 @@
-import 'select2';
+require('./app.css');
 
+import 'select2';
 jQuery("document").ready(function() {
     jQuery(".mailselect").select2({
         tags: true,
         tokenSeparators: [',', ' ']
     })
 });
+

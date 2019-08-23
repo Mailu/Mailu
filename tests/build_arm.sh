e1a4bf32a6b56462c5f44fa44ac8a81a457a9496
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-08-23 15:24:21 -0400

    only insert qemu binary if building for arm

--- tests/build_arm.sh
+++ tests/build_arm.sh
@@ -3,11 +3,13 @@
 ALPINE_VER="3.10"
 DISTRO="balenalib/rpi-alpine:$ALPINE_VER"
 # Used for webmails
-PHP="arm32v7/php:7.3-apache"
+QEMU="arm"
+ARCH="arm32v7/"
 
 # use qemu-*-static from docker container
 docker run --rm --privileged multiarch/qemu-user-static:register 
 docker-compose -f build.yml build \
   --build-arg DISTRO=$DISTRO \
-  --build-arg PHP_DISTRO=$PHP \
+  --build-arg ARCH=$ARCH \
+  --build-arg QEMU=$QEMU \
   --parallel $@

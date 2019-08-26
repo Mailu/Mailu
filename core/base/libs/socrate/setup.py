f63837b8e162188d8b6a95a07bc364a31891566f
kaiyou <pierre@jaury.eu>
2019-08-26 22:44:55 +0200

    Update to 0.2.0

--- core/base/libs/socrate/setup.py
+++ core/base/libs/socrate/setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="socrate",
-    version="0.1.1",
+    version="0.2.0",
     description="Socrate daemon utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",

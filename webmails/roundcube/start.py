8f3fb155dd7178f352c6b5cc6e12a9b0727d441c
Michael Wyraz <michael@wyraz.de>
2019-11-19 12:22:09 +0100

    Make roundcube db configurable

--- webmails/roundcube/start.py
+++ webmails/roundcube/start.py
@@ -9,6 +9,29 @@ log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))
 
 os.environ["MAX_FILESIZE"] = str(int(int(os.environ.get("MESSAGE_SIZE_LIMIT"))*0.66/1048576))
 
+db_flavor=os.environ.get("ROUNDCUBE_DB_FLAVOR","sqlite")
+if db_flavor=="sqlite":
+    os.environ["DB_DSNW"]="sqlite:////data/roundcube.db"
+elif db_flavor=="mysql":
+    os.environ["DB_DSNW"]="mysql://%s:%s@%s/%s" % (
+        os.environ.get("ROUNDCUBE_DB_USER","roundcube"),
+        os.environ.get("ROUNDCUBE_DB_PW"),
+        os.environ.get("ROUNDCUBE_DB_HOST","database"),
+        os.environ.get("ROUNDCUBE_DB_NAME","roundcube")
+        )
+elif db_flavor=="postgresql":
+    os.environ["DB_DSNW"]="pgsql://%s:%s@%s/%s" % (
+        os.environ.get("ROUNDCUBE_DB_USER","roundcube"),
+        os.environ.get("ROUNDCUBE_DB_PW"),
+        os.environ.get("ROUNDCUBE_DB_HOST","database"),
+        os.environ.get("ROUNDCUBE_DB_NAME","roundcube")
+        )
+else:
+    print("Unknown ROUNDCUBE_DB_FLAVOR: %s",db_flavor)
+    exit(1)
+
+
+
 conf.jinja("/php.ini", os.environ, "/usr/local/etc/php/conf.d/roundcube.ini")
 
 # Fix some permissions

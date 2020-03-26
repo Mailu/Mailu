eebc545c7bd1eda525c0e7cdb292d94652252bbd
ofthesun9 <olivier@ofthesun.net>
2020-03-26 13:46:43 +0100

    Script to be used to define MAILU_VERSION for the testing branch Derived from tests/deploy.sh This script shall be used in every travis step (before push/pull) for naming consistency.

---
+++ tests/define_MAILU_VERSION.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Retag in case of `bors try`
+if [ "$TRAVIS_BRANCH" = "testing" ]; then
+    export DOCKER_ORG="mailutest"
+    # Commit message is like "Try #99".
+    # This sets the version tag to "pr-99"
+    export MAILU_VERSION="pr-${TRAVIS_COMMIT_MESSAGE//[!0-9]/}"
+    docker-compose -f tests/build.yml build
+fi

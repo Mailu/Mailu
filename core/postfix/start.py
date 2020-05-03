ae52a6e37f5e70b580dee21126fecd45f677044b
ofthesun9 <olivier@ofthesun.net>
2020-05-03 19:13:06 +0200

    Modify start.py to use debian as base image Change uid to 101. Change path to post-install script

--- core/postfix/start.py
+++ core/postfix/start.py
@@ -13,7 +13,7 @@ from socrate import system, conf
 log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))
 
 def start_podop():
-    os.setuid(100)
+    os.setuid(101)
     url = "http://" + os.environ["ADMIN_ADDRESS"] + "/internal/postfix/"
     # TODO: Remove verbosity setting from Podop?
     run_server(0, "postfix", "/tmp/podop.socket", [
@@ -63,5 +63,5 @@ if "RELAYUSER" in os.environ:
 
 # Run Podop and Postfix
 multiprocessing.Process(target=start_podop).start()
-os.system("/usr/libexec/postfix/post-install meta_directory=/etc/postfix create-missing")
+os.system("/etc/postfix/post-install meta_directory=/etc/postfix create-missing")
 os.system("postfix start-fg")

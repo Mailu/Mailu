43e62d5b7bf9beeca3ff602a0c1f1808127f7d69
Florent Daigniere <nextgens@freenetproject.org>
2021-01-29 19:42:22 +0100

    PEP8 compliance

--- core/admin/mailu/internal/views/auth.py
+++ core/admin/mailu/internal/views/auth.py
@@ -10,51 +10,41 @@ import ipaddress
 def extract_network_from_ip(ip):
     n = ipaddress.ip_network(ip)
     if isinstance(n, ipaddress.IPv4Network):
-        # /24
         return str(n.supernet(prefixlen_diff=(32-int(app.config["AUTH_RATELIMIT_IP_V4_MASK"]))).network_address)
     elif isinstance(n, ipaddress.IPv6Network):
-        # /56
         return str(n.supernet(prefixlen_diff=(128-int(app.config["AUTH_RATELIMIT_IP_V6_MASK"]))).network_address)
     else: # not sure what to do with it
         return ip
 
-def isExempt(ip):
+def is_subject_to_rate_limits(ip):
     key = "exempt-{}".format(ip)
-    return utils.limiter.storage.get(key) > 0
+    return not (utils.limiter.storage.get(key) > 0)
 
-def exemptIP(ip):
+def exempt_ip_from_ratelimits(ip):
     key = "exempt-{}".format(ip)
     utils.limiter.storage.incr(key, app.config["AUTH_RATELIMIT_EXEMPTION_LENGTH"], True)
 
-def shouldRateLimitSubnet(ip):
-    """ Tell us whether we should take rate-limiting into account for this IP
-    """
+def should_rate_limit_ip(ip):
     limiter = utils.limiter.get_limiter(app.config["AUTH_RATELIMIT_IP"], "auth-ip")
     client_network = extract_network_from_ip(ip)
-    return not isExempt(ip) and not limiter.test(client_network)
+    return is_subject_to_rate_limits(ip) and not limiter.test(client_network)
 
-def rateLimitSubnet(ip):
-    """ Increase the hit count to ensure that subsequent attempts can be rate-limited
-    """
+def rate_limit_ip(ip):
     limit_subnet = str(app.config["AUTH_RATELIMIT_SUBNET"]) != 'False'
     subnet = ipaddress.ip_network(app.config["SUBNET"])
     if limit_subnet or ipaddress.ip_address(ip) not in subnet:
         limiter = utils.limiter.get_limiter(app.config["AUTH_RATELIMIT_IP"], "auth-ip")
         client_network = extract_network_from_ip(ip)
-        if not isExempt(ip):
+        if is_subject_to_rate_limits(ip):
             limiter.hit(client_network)
 
-def shouldRateLimitUser(username, ip):
-    """ Tell us whether we should take rate-limiting into account for this username
-    """
+def should_rate_limit_user(username, ip):
     limiter = utils.limiter.get_limiter(app.config["AUTH_RATELIMIT_USER"], "auth-user")
-    return not isExempt(ip) and not limiter.test(username)
+    return is_subject_to_rate_limits(ip) and not limiter.test(username)
 
-def rateLimitUser(username, ip):
-    """ Increase the hit count to ensure that subsequent attempts can be rate-limited
-    """
+def rate_limit_user(username, ip):
     limiter = utils.limiter.get_limiter(app.config["AUTH_RATELIMIT_USER"], "auth-user")
-    if not isExempt(ip):
+    if is_subject_to_rate_limits(ip):
         limiter.hit(username)
 
 @internal.route("/auth/email")
@@ -62,7 +52,7 @@ def nginx_authentication():
     """ Main authentication endpoint for Nginx email server
     """
     client_ip = flask.request.headers["Client-Ip"]
-    if shouldRateLimitSubnet(client_ip):
+    if should_rate_limit_ip(client_ip):
         response = flask.Response()
         response.headers['Auth-Status'] = 'Authentication rate limit from one source exceeded'
         response.headers['Auth-Error-Code'] = '451 4.3.2'
@@ -74,25 +64,22 @@ def nginx_authentication():
     for key, value in headers.items():
         response.headers[key] = str(value)
 
-    isValidUser = False
+    is_valid_user = False
     if "Auth-User-Exists" in response.headers and response.headers["Auth-User-Exists"] == "True":
         username = response.headers["Auth-User"]
-        if shouldRateLimitUser(username, client_ip):
-            # FIXME should be done before the handle_authentication
+        if should_rate_limit_user(username, client_ip):
+            # FIXME could be done before handle_authentication()
             response = flask.Response()
             response.headers['Auth-Status'] = 'Authentication rate limit for this username exceeded'
             response.headers['Auth-Error-Code'] = '451 4.3.2'
             if int(flask.request.headers['Auth-Login-Attempt']) < 10:
                 response.headers['Auth-Wait'] = '3'
             return response
-        isValidUser = True
+        is_valid_user = True
     if ("Auth-Status" not in headers) or (headers["Auth-Status"] != "OK"):
-        if isValidUser:
-            rateLimitUser(username, client_ip)
-        else:
-            rateLimitSubnet(client_ip)
+        rate_limit_user(username, client_ip) if is_valid_user else rate_limit_ip(client_ip)
     elif ("Auth-Status" in headers) and (headers["Auth-Status"] == "OK"):
-        exemptIP(client_ip)
+        exempt_ip_from_ratelimits(client_ip)
     return response
 
 
@@ -112,7 +99,7 @@ def basic_authentication():
     """ Tries to authenticate using the Authorization header.
     """
     client_ip = flask.request.headers["X-Real-IP"] if 'X-Real-IP' in flask.request.headers else flask.request.remote_addr
-    if shouldRateLimitSubnet(client_ip):
+    if should_rate_limit_ip(client_ip):
         response = flask.Response(status=401)
         response.headers["WWW-Authenticate"] = 'Basic realm="Authentication rate limit from one source exceeded"'
         response.headers['Retry-After'] = '60'
@@ -122,7 +109,7 @@ def basic_authentication():
         encoded = authorization.replace("Basic ", "")
         user_email, password = base64.b64decode(encoded).split(b":")
         user_email = user_email.decode("utf8")
-        if shouldRateLimitUser(user_email, client_ip):
+        if should_rate_limit_user(user_email, client_ip):
             response = flask.Response(status=401)
             response.headers["WWW-Authenticate"] = 'Basic realm="Authentication rate limit for this username exceeded"'
             response.headers['Retry-After'] = '60'
@@ -142,11 +129,11 @@ def basic_authentication():
             if status:
                 response = flask.Response()
                 response.headers["X-User"] = user.email
-                exemptIP(client_ip)
+                exempt_ip_from_ratelimits(client_ip)
                 return response
-            rateLimitUser(user_email, client_ip)
+            rate_limit_user(user_email, client_ip)
         else:
-            rateLimitSubnet(client_ip)
+            rate_limit_ip(client_ip)
     response = flask.Response(status=401)
     response.headers["WWW-Authenticate"] = 'Basic realm="Login Required"'
     return response

368802ed60aabac6ed0fab2422079bfb1f444e05
Florent Daigniere <nextgens@freenetproject.org>
2021-02-06 08:47:48 +0100

    Revert "Enable reCAPTCHA for rainloop if configured"
    This reverts commit 71b4aad6b9647d16fe8b98d9b84af8b9b3b9b42b.

--- webmails/rainloop/start.py
+++ webmails/rainloop/start.py
@@ -5,7 +5,6 @@ import shutil
 import logging as log
 import sys
 from socrate import system, conf
-from zipfile import ZipFile
 
 log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))
 
@@ -19,14 +18,9 @@ base = "/data/_data_/_default_/"
 shutil.rmtree(base + "domains/", ignore_errors=True)
 os.makedirs(base + "domains", exist_ok=True)
 os.makedirs(base + "configs", exist_ok=True)
-os.makedirs(base + "plugins", exist_ok=True)
-
-with ZipFile('/var/www/recaptcha.zip', 'r') as zf:
-    zf.extractall('/data/_data_/_default_/plugins/')
 
 conf.jinja("/default.ini", os.environ, "/data/_data_/_default_/domains/default.ini")
 conf.jinja("/application.ini", os.environ, "/data/_data_/_default_/configs/application.ini")
-conf.jinja("/plugin-recaptcha.ini", os.environ, "/data/_data_/_default_/configs/plugin-recaptcha.ini")
 conf.jinja("/php.ini", os.environ, "/usr/local/etc/php/conf.d/rainloop.ini")
 
 os.system("chown -R www-data:www-data /data")

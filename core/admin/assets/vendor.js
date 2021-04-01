7d3c9d412d430d7fbd578df1cb738126f7e1c53e
DjVinnii <vincentkling@msn.com>
2021-04-01 16:05:30 +0200

    Change tables to datatables

--- core/admin/assets/vendor.js
+++ core/admin/assets/vendor.js
@@ -14,6 +14,8 @@ import 'admin-lte/plugins/fontawesome-free/css/solid.css';
 
 // AdminLTE
 import 'admin-lte/build/scss/adminlte.scss';
+import 'admin-lte/plugins/datatables-bs4/css/dataTables.bootstrap4.css';
+import 'admin-lte/plugins/datatables-responsive/css/responsive.bootstrap4.css';
 // import 'admin-lte/build/less/AdminLTE-without-plugins.less'; // doesn't exist
 // import 'admin-lte/build/less/select2.less'; // doesn't exist
 // import 'admin-lte/build/less/skins/skin-blue.less'; // doesn't exist

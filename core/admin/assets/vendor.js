6b3170cb4c1f9bcda28c25b7c27849cc1de06f7f
DjVinnii <vincentkling@msn.com>
2021-04-01 14:42:15 +0200

    Update side menu

--- core/admin/assets/vendor.js
+++ core/admin/assets/vendor.js
@@ -3,17 +3,21 @@ import jQuery from 'jquery';
 import 'select2/dist/css/select2.css';
 
 // bootstrap
-import 'bootstrap/less/bootstrap.less';
-import 'bootstrap';
+// import 'bootstrap/less/bootstrap.less';
+// import 'bootstrap';
 
 // FA
-import 'font-awesome/scss/font-awesome.scss';
+// import 'font-awesome/scss/font-awesome.scss'; // FA is included with AdminLTE
+import 'admin-lte/plugins/fontawesome-free/css/fontawesome.css';
+import 'admin-lte/plugins/fontawesome-free/css/regular.css';
+import 'admin-lte/plugins/fontawesome-free/css/solid.css';
 
 // AdminLTE
-import 'admin-lte/build/less/AdminLTE-without-plugins.less';
-import 'admin-lte/build/less/select2.less';
-import 'admin-lte/build/less/skins/skin-blue.less';
+import 'admin-lte/build/scss/adminlte.scss';
+// import 'admin-lte/build/less/AdminLTE-without-plugins.less'; // doesn't exist
+// import 'admin-lte/build/less/select2.less'; // doesn't exist
+// import 'admin-lte/build/less/skins/skin-blue.less'; // doesn't exist
 import 'admin-lte/build/js/Layout.js';
 import 'admin-lte/build/js/ControlSidebar.js';
 import 'admin-lte/build/js/PushMenu.js';
-import 'admin-lte/build/js/BoxRefresh.js';
+// import 'admin-lte/build/js/BoxRefresh.js'; // doesn't exist

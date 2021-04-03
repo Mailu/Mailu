f30cca1263f501fc1ec144ad4a9ff43c65e2e952
DjVinnii <vincentkling@msn.com>
2021-04-03 10:32:01 +0200

    Do imports based on AdminLTE plugins

--- core/admin/assets/app.js
+++ core/admin/assets/app.js
@@ -1,6 +1,6 @@
 require('./app.css');
 
-import 'select2';
+import 'admin-lte/plugins/select2/js/select2.js';
 import 'admin-lte/plugins/datatables/jquery.dataTables.js';
 import 'admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.js';
 import 'admin-lte/plugins/datatables-responsive/js/dataTables.responsive.js';
--- core/admin/assets/vendor.js
+++ core/admin/assets/vendor.js
@@ -1,13 +1,12 @@
 // jQuery
 import jQuery from 'jquery';
-import 'select2/dist/css/select2.css';
+import 'admin-lte/plugins/select2/css/select2.css';
 
 // bootstrap
 // import 'bootstrap/less/bootstrap.less';
 // import 'bootstrap';
 
-// FA
-// import 'font-awesome/scss/font-awesome.scss'; // FA is included with AdminLTE
+// FontAwesome
 import 'admin-lte/plugins/fontawesome-free/css/fontawesome.css';
 import 'admin-lte/plugins/fontawesome-free/css/regular.css';
 import 'admin-lte/plugins/fontawesome-free/css/solid.css';
@@ -16,10 +15,6 @@ import 'admin-lte/plugins/fontawesome-free/css/solid.css';
 import 'admin-lte/build/scss/adminlte.scss';
 import 'admin-lte/plugins/datatables-bs4/css/dataTables.bootstrap4.css';
 import 'admin-lte/plugins/datatables-responsive/css/responsive.bootstrap4.css';
-// import 'admin-lte/build/less/AdminLTE-without-plugins.less'; // doesn't exist
-// import 'admin-lte/build/less/select2.less'; // doesn't exist
-// import 'admin-lte/build/less/skins/skin-blue.less'; // doesn't exist
 import 'admin-lte/build/js/Layout.js';
 import 'admin-lte/build/js/ControlSidebar.js';
 import 'admin-lte/build/js/PushMenu.js';
-// import 'admin-lte/build/js/BoxRefresh.js'; // doesn't exist

0a049b95e5b816efc3627ff858bfc8afb8094bcb
Dimitri Huisman <diman@huisman.xyz>
2021-07-04 12:33:53 +0000

    Sync PR with master CI changes

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -5,7 +5,7 @@
 
 # Retag in case of `bors try`
 if [ "$TRAVIS_BRANCH" = "testing" ]; then
-    export DOCKER_ORG="mailutest"
+    export DOCKER_ORG=$DOCKER_ORG_TESTS
     # Commit message is like "Try #99".
     # This sets the version tag to "pr-99"
     export MAILU_VERSION="pr-${TRAVIS_COMMIT_MESSAGE//[!0-9]/}"

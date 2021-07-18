d7931ed5af1999ec88118a78de252de5c9515fd2
Florent Daigniere <nextgens@freenetproject.org>
2021-07-18 18:43:20 +0200

    The lookup could fail; ensure we set something
    (cherry picked from commit 1d65529c94f54de3cb49ed9584ed95f7860c26fa)

--- core/postfix/start.py
+++ core/postfix/start.py
@@ -37,7 +37,10 @@ os.environ["ADMIN_ADDRESS"] = system.get_host_address_from_environment("ADMIN", 
 os.environ["ANTISPAM_MILTER_ADDRESS"] = system.get_host_address_from_environment("ANTISPAM_MILTER", "antispam:11332")
 os.environ["LMTP_ADDRESS"] = system.get_host_address_from_environment("LMTP", "imap:2525")
 os.environ["OUTCLEAN"] = os.environ["HOSTNAMES"].split(",")[0]
-os.environ["OUTCLEAN_ADDRESS"] = system.resolve_hostname(os.environ["OUTCLEAN"])
+try:
+    os.environ["OUTCLEAN_ADDRESS"] = system.resolve_hostname(os.environ["OUTCLEAN"])
+except:
+    os.environ["OUTCLEAN_ADDRESS"] = "10.10.10.10"
 
 for postfix_file in glob.glob("/conf/*.cf"):
     conf.jinja(postfix_file, os.environ, os.path.join("/etc/postfix", os.path.basename(postfix_file)))

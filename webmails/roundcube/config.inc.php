10f2c179797f86e29f3e1e0901c1c2e60b728701
Erriez <Erriez@users.noreply.github.com>
2021-08-13 23:21:12 +0200

    Restore Roundcube PHP files

---
+++ webmails/roundcube/config.inc.php
@@ -0,0 +1,66 @@
+<?php
+
+$config = array();
+
+// Generals
+$config['db_dsnw'] = getenv('DB_DSNW');;
+$config['temp_dir'] = '/tmp/';
+$config['des_key'] = getenv('SECRET_KEY') ? getenv('SECRET_KEY') : trim(file_get_contents(getenv('SECRET_KEY_FILE')));
+$config['cipher_method'] = 'AES-256-CBC';
+$config['identities_level'] = 0;
+$config['reply_all_mode'] = 1;
+
+// List of active plugins (in plugins/ directory)
+$config['plugins'] = array(
+    'archive',
+    'zipdownload',
+    'markasjunk',
+    'managesieve',
+    'enigma',
+    'carddav'
+);
+
+$front = getenv('FRONT_ADDRESS') ? getenv('FRONT_ADDRESS') : 'front';
+$imap  = getenv('IMAP_ADDRESS')  ? getenv('IMAP_ADDRESS')  : 'imap';
+
+// Mail servers
+$config['default_host'] = $front;
+$config['default_port'] = 10143;
+$config['smtp_server'] = $front;
+$config['smtp_port'] = 10025;
+$config['smtp_user'] = '%u';
+$config['smtp_pass'] = '%p';
+
+// Sieve script management
+$config['managesieve_host'] = $imap;
+$config['managesieve_usetls'] = false;
+
+// Customization settings
+if (filter_var(getenv('ADMIN'), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE)) {
+	array_push($config['plugins'], 'mailu');
+	$config['support_url'] = getenv('WEB_ADMIN') ? '../..' . getenv('WEB_ADMIN') : '';
+	$config['sso_logout_url'] = getenv('WEB_ADMIN').'/ui/logout';
+}
+$config['product_name'] = 'Mailu Webmail';
+
+// We access the IMAP and SMTP servers locally with internal names, SSL
+// will obviously fail but this sounds better than allowing insecure login
+// from the outter world
+$ssl_no_check = array(
+ 'ssl' => array(
+     'verify_peer' => false,
+     'verify_peer_name' => false,
+  ),
+);
+$config['imap_conn_options'] = $ssl_no_check;
+$config['smtp_conn_options'] = $ssl_no_check;
+$config['managesieve_conn_options'] = $ssl_no_check;
+
+// skin name: folder from skins/
+$config['skin'] = 'elastic';
+
+// Enigma gpg plugin
+$config['enigma_pgp_homedir'] = '/data/gpg';
+
+// Set From header for DKIM signed message delivery reports
+$config['mdn_use_from'] = true;

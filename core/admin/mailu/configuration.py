e5972bd9ecdb81c22998a380311add36538caea1
Dimitri Huisman <diman@huisman.xyz>
2021-08-18 15:01:10 +0000

    Set default message rate limit to 200/day

--- core/admin/mailu/configuration.py
+++ core/admin/mailu/configuration.py
@@ -46,7 +46,7 @@ DEFAULT_CONFIG = {
     'DKIM_SELECTOR': 'dkim',
     'DKIM_PATH': '/dkim/{domain}.{selector}.key',
     'DEFAULT_QUOTA': 1000000000,
-    'MESSAGE_RATELIMIT': '100/hour',
+    'MESSAGE_RATELIMIT': '200/day',
     # Web settings
     'SITENAME': 'Mailu',
     'WEBSITE': 'https://mailu.io',

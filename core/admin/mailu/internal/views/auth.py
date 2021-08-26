4f5cb0974ef4a776c43893658fc6e779468bc3b6
Dimitri Huisman <diman@huisman.xyz>
2021-08-26 15:11:35 +0000

    Make sure HTTP header only contains ASCII

--- core/admin/mailu/internal/views/auth.py
+++ core/admin/mailu/internal/views/auth.py
@@ -50,7 +50,7 @@ def user_authentication():
     if (not flask_login.current_user.is_anonymous
         and flask_login.current_user.enabled):
         response = flask.Response()
-        response.headers["X-User"] = flask_login.current_user.get_id()
+        response.headers["X-User"] = models.IdnaEmail.process_bind_param(flask_login, flask_login.current_user.get_id(), "")
         response.headers["X-User-Token"] = models.User.get_temp_token(flask_login.current_user.get_id())
         return response
     return flask.abort(403)

74dcfaaf21c1ba98b1105449cc26e0310641e8fb
Alexander Graf <ghostwheel42@users.noreply.github.com>
2021-10-08 19:54:31 +0200

    moved javascript to app.js
    (cherry picked from commit aaf3ddd002b87dfccd09e82a00830364a19aeed1)
    
    # Conflicts:
    #	core/admin/assets/app.js
    #	core/admin/mailu/ui/templates/login.html

--- core/admin/assets/app.js
+++ core/admin/assets/app.js
@@ -4,7 +4,32 @@ import 'select2';
 jQuery("document").ready(function() {
     jQuery(".mailselect").select2({
         tags: true,
+<<<<<<< HEAD
         tokenSeparators: [',', ' ']
     })
+=======
+        tokenSeparators: [',', ' '],
+    });
+
+    // init dataTable
+    var d = $(document.documentElement);
+    $('.dataTable').DataTable({
+        'responsive': true,
+        language: {
+            url: d.data('static') + d.attr('lang') + '.json',
+        },
+    });
+
+    // init clipboard.js
+    new ClipboardJS('.btn-clip');
+
+    // disable login if not possible
+    var l = $('#login_needs_https');
+    if (l.length && window.location.protocol != 'https:') {
+        l.removeClass("d-none");
+        $('form :input').prop('disabled', true);
+    }
+
+>>>>>>> aaf3ddd0 (moved javascript to app.js)
 });
 

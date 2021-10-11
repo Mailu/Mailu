4eb5f4b5f31cbe0b0e85e8f7f9262421b4a27adc
Florent Daigniere <nextgens@freenetproject.org>
2021-10-11 11:27:01 +0200

    merge

--- core/admin/assets/app.js
+++ core/admin/assets/app.js
@@ -4,24 +4,8 @@ import 'select2';
 jQuery("document").ready(function() {
     jQuery(".mailselect").select2({
         tags: true,
-<<<<<<< HEAD
-        tokenSeparators: [',', ' ']
-    })
-=======
         tokenSeparators: [',', ' '],
     });
-
-    // init dataTable
-    var d = $(document.documentElement);
-    $('.dataTable').DataTable({
-        'responsive': true,
-        language: {
-            url: d.data('static') + d.attr('lang') + '.json',
-        },
-    });
-
-    // init clipboard.js
-    new ClipboardJS('.btn-clip');
 
     // disable login if not possible
     var l = $('#login_needs_https');
@@ -29,7 +13,5 @@ jQuery("document").ready(function() {
         l.removeClass("d-none");
         $('form :input').prop('disabled', true);
     }
-
->>>>>>> aaf3ddd0 (moved javascript to app.js)
 });
 

92e65b33e03ba0bc881db28a7412489c572b732e
Dimitri Huisman <diman@huisman.xyz>
2021-11-10 09:07:03 +0000

    Configure fetchmail to use idfile to keep track of messages. Run fetchmail as root. This is unfortunately required because all files are owned by root in the mailu data folder. In the future  we must switch all images to running all all processes with a non-root user.

--- optional/fetchmail/fetchmail.py
+++ optional/fetchmail/fetchmail.py
@@ -13,7 +13,7 @@ import traceback
 
 FETCHMAIL = """
 fetchmail -N \
-    --idfile /data/.fetchids \
+    --idfile /data/.fetchids --uidl \
     --sslcertck --sslcertpath /etc/ssl/certs \
     -f {}
 """

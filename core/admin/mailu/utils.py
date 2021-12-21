bee6e980e31c2f2a88c1ea74b901822324f2d903
Florent Daigniere <nextgens@freenetproject.org>
2021-12-21 16:23:27 +0100

    doh

--- core/admin/mailu/utils.py
+++ core/admin/mailu/utils.py
@@ -490,7 +490,7 @@ session = MailuSessionExtension()
 def verify_temp_token(email, token):
     try:
         if token.startswith('token-'):
-            if sessid := app.session_store.get(token)
+            if sessid := app.session_store.get(token):
                 session = MailuSession(sessid, app)
                 if session.get('_user_id', '') == email:
                     return True

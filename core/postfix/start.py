89a86e9dda8cc012dd73ec84d0b2f09d5375f070
Sebastian Klemke <3669903+packet23@users.noreply.github.com>
2022-01-22 17:15:51 +0100

    disabled rsyslogd pidfile

--- core/postfix/start.py
+++ core/postfix/start.py
@@ -85,7 +85,7 @@ if "RELAYUSER" in os.environ:
 
 # Configure and start local rsyslog server
 conf.jinja("/conf/rsyslog.conf", os.environ, "/etc/rsyslog.conf")
-os.system("/usr/sbin/rsyslogd -n &")
+os.system("/usr/sbin/rsyslogd -niNONE &")
 # Configure logrotate and start crond
 if os.environ["POSTFIX_LOG_FILE"] != "":
     conf.jinja("/conf/logrotate.conf", os.environ, "/etc/logrotate.d/postfix.conf")

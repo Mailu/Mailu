85d710ba2b1c5cb34603c4b835b7679a59f5f6f2
mnival <1595998+mnival@users.noreply.github.com>
2022-01-28 16:02:27 +0100

    Add /overrides directory in include
    (cherry picked from commit 4b9781210f10f9730ce70e24ad47b3616ee359f4)
    
    # Conflicts:
    #	webmails/roundcube/config.inc.php

---
+++ webmails/roundcube/config.inc.php
@@ -0,0 +1,66 @@
+<?php
+
+$config = array();
+
+// Generals
+$config['db_dsnw'] = '{{ DB_DSNW }}';
+$config['temp_dir'] = '/tmp/';
+$config['des_key'] = '{{ SECRET_KEY }}';
+$config['cipher_method'] = 'AES-256-CBC';
+$config['identities_level'] = 0;
+$config['reply_all_mode'] = 1;
+$config['log_driver'] = 'stdout';
+$config['zipdownload_selection'] = true;
+$config['enable_spellcheck'] = true;
+$config['spellcheck_engine'] = 'pspell';
+$config['session_lifetime'] = {{ SESSION_TIMEOUT_MINUTES | int }};
+
+// Mail servers
+$config['default_host'] = '{{ FRONT_ADDRESS or "front" }}';
+$config['default_port'] = 10143;
+$config['smtp_server'] = '{{ FRONT_ADDRESS or "front" }}';
+$config['smtp_port'] = 10025;
+$config['smtp_user'] = '%u';
+$config['smtp_pass'] = '%p';
+
+// Sieve script management
+$config['managesieve_host'] = '{{ IMAP_ADDRESS or "imap" }}';
+$config['managesieve_usetls'] = false;
+
+// We access the IMAP and SMTP servers locally with internal names, SSL
+// will obviously fail but this sounds better than allowing insecure login
+// from the outter world
+$ssl_no_check = array(
+ 'ssl' => array(
+     'verify_peer' => false,
+     'verify_peer_name' => false,
+  ),
+);
+$config['imap_conn_options'] = $ssl_no_check;
+$config['smtp_conn_options'] = $ssl_no_check;
+$config['managesieve_conn_options'] = $ssl_no_check;
+
+// roundcube customization
+$config['product_name'] = 'Mailu Webmail';
+{%- if ADMIN and WEB_ADMIN %}
+$config['support_url'] = '../..{{ WEB_ADMIN }}';
+{%- endif %}
+$config['plugins'] = array({{ PLUGINS }});
+
+// skin name: folder from skins/
+$config['skin'] = 'elastic';
+
+// configure mailu sso plugin
+$config['sso_logout_url'] = '/sso/logout';
+
+// configure enigma gpg plugin
+$config['enigma_pgp_homedir'] = '/data/gpg';
+
+// set From header for DKIM signed message delivery reports
+$config['mdn_use_from'] = true;
+
+// includes
+{%- for inc in INCLUDES %}
+include('/overrides/{{ inc }}');
+{%- endfor %}
+

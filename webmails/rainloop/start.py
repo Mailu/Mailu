b24a2d9a6e2f87e6c71a705da426b5c1790788d9
Mario Jauvin <marioja@users.noreply.github.com>
2022-01-31 12:19:15 -0500

    Added subprocess import

--- webmails/rainloop/start.py
+++ webmails/rainloop/start.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import logging as log
 import sys
+import subprocess
 from socrate import system, conf
 
 log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))

ab35492589ea5f0fead61e90e3b3a95927a608ed
Florent Daigniere <nextgens@freenetproject.org>
2022-02-20 12:02:30 +0100

    the first time the loop runs we don't have the second cert

--- core/nginx/config.py
+++ core/nginx/config.py
@@ -36,6 +36,8 @@ args["TLS"] = {
 
 def format_for_nginx(fullchain, output):
     """ We may want to strip ISRG Root X1 out """
+    if not os.path.exists(fullchain):
+        return
     certs = []
     with open(fullchain, 'r') as pem:
         cert = ''

084e2428b445f6b247073a6c3d35ddb0672738e2
Sebastian Wilke <sebastian.wilke@wilkebosin.ventures>
2022-06-05 22:43:31 +0200

    Init oic client when enabled

--- core/admin/mailu/__init__.py
+++ core/admin/mailu/__init__.py
@@ -29,6 +29,8 @@ def create_app_from_config(config):
     utils.migrate.init_app(app, models.db)
     if app.config["KEYCLOAK_ENABLED"]:
         utils.keycloak_client.init_app(app)
+    if app.config["OIDC_ENABLED"]:
+        utils.oic_client.init_app(app)
 
     app.device_cookie_key = hmac.new(bytearray(app.secret_key, 'utf-8'), bytearray('DEVICE_COOKIE_KEY', 'utf-8'), 'sha256').digest()
     app.temp_token_key = hmac.new(bytearray(app.secret_key, 'utf-8'), bytearray('WEBMAIL_TEMP_TOKEN_KEY', 'utf-8'), 'sha256').digest()

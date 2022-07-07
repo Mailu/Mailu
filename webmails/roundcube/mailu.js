bb7febd2b329bba2f58c9329e40add2051993b61
ItzYanick <me@yanick.gay>
2022-07-08 00:30:19 +0200

    requested changes

--- webmails/roundcube/mailu.js
+++ webmails/roundcube/mailu.js
@@ -1,9 +1,11 @@
-rcmail.addEventListener('init', function(evt) {
-      var btn = document.createElement('a');
-      btn.setAttribute("href", "{{ WEB_ADMIN }}");
-      btn.setAttribute("role", "button");
-      btn.setAttribute("class", "showurl");
-      btn.innerHTML = '<span class="inner">To Mailu</span>';
+if ({{ ADMIN }}) {
+      rcmail.addEventListener('init', function(evt) {
+            var btn = document.createElement('a');
+            btn.setAttribute("href", "{{ WEB_ADMIN }}");
+            btn.setAttribute("role", "button");
+            btn.setAttribute("class", "showurl");
+            btn.innerHTML = '<span class="inner">To {{ SITENAME }}</span>';
 
-      document.getElementById("taskmenu").appendChild(btn);
-});
+            document.getElementById("taskmenu").appendChild(btn);
+      });
+}

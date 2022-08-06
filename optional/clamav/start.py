7fca87db565d1743e82bf7ee5cfeca82ea0890bd
Dimitri Huisman <diman@huisman.xyz>
2022-08-06 09:52:48 +0000

    The MESSAGE_SIZE_LIMIT is now used for the clamav configuration (MaxScanSize, MaxFileSize). This resolves the situation where MESSAGE_SIZE_LIMIT was set higher than the hard-coded values in the clamav configuration.

--- optional/clamav/start.py
+++ optional/clamav/start.py
@@ -3,9 +3,15 @@
 import os
 import logging as log
 import sys
+import glob
+from socrate import conf
 
 log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))
 logger=log.getLogger(__name__)
+
+# Parse configuration files
+for clamav_file in glob.glob("/conf/*.conf"):
+    conf.jinja(clamav_file, os.environ, os.path.join("/etc/clamav", os.path.basename(clamav_file)))
 
 # Bootstrap the database if clamav is running for the first time
 if not os.path.isfile("/data/main.cvd"):

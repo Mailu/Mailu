1379a583525351458d2675d3fc81840eec7f73fe
Florent Daigniere <nextgens@freenetproject.org>
2022-11-12 14:50:30 +0100

    Basic hardening

--- tests/compose/webmail/01_ensure_admin_unreachable.sh
+++ tests/compose/webmail/01_ensure_admin_unreachable.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-[[ `curl -I -so /dev/null -w "%{http_code}" http://localhost/` -ne 200 ]] && echo "The default page of rainloop hasn't returned 200!" >>/dev/stderr && exit 1
-[[ `curl -I -so /dev/null -w "%{http_code}" http://localhost/?admin` -ne 403 ]] && echo "The admin of rainloop is not disabled!" >>/dev/stderr && exit 1
+[[ `curl -I -so /dev/null -w "%{http_code}" http://localhost/` -ne 200 ]] && echo "The default page of snappymail hasn't returned 200!" >>/dev/stderr && exit 1
+[[ `curl -I -so /dev/null -w "%{http_code}" http://localhost/?admin` -ne 403 ]] && echo "The admin of snappymail is not disabled!" >>/dev/stderr && exit 1

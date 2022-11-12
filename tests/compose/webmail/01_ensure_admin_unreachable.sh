6d8cc9083bbc44a84e8412c61cf539b6ce356f4e
Florent Daigniere <nextgens@freenetproject.org>
2022-11-12 15:21:04 +0100

    test

--- tests/compose/webmail/01_ensure_admin_unreachable.sh
+++ tests/compose/webmail/01_ensure_admin_unreachable.sh
@@ -2,5 +2,8 @@
 
 IP="$(docker inspect webmail_webmail_1|jq '.[0].NetworkSettings.Networks.webmail_default.IPAddress')"
 
+echo "webmail is on $IP"
+wget -S http://$IP/ &>/dev/stderr
+
 [[ $(curl -I -so /dev/null -w "%{http_code}" http://$IP/) -ne 200 ]] && echo "The default page of snappymail hasn't returned 200!" >>/dev/stderr && exit 1
 [[ $(curl -I -so /dev/null -w "%{http_code}" http://$IP/?admin) -ne 403 ]] && echo "The admin of snappymail is not disabled!" >>/dev/stderr && exit 1

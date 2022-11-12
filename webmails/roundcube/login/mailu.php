ad17b10c8e208338c2001b86d7604b8c8a883b89
Florent Daigniere <nextgens@freenetproject.org>
2022-11-12 15:31:47 +0100

    redirects should be HTTP/302

--- webmails/roundcube/login/mailu.php
+++ webmails/roundcube/login/mailu.php
@@ -28,7 +28,7 @@ class mailu extends rcube_plugin
         header('HTTP/1.0 403 Forbidden');
         print('mailu sso failure');
       } else {
-        header('Location: sso.php');
+        header('Location: sso.php', 302);
       }
       exit();
     }
@@ -47,19 +47,19 @@ class mailu extends rcube_plugin
   {
     $this->load_config();
     $sso_logout_url = rcmail::get_instance()->config->get('sso_logout_url');
-    header('Location: ' . $sso_logout_url, true);
+    header('Location: ' . $sso_logout_url, true, 302);
     exit();
   }
 
   function login($args)
   {
-    header('Location: index.php');
+    header('Location: index.php', 302);
     exit();
   }
 
   function login_failed($args)
   {
-    header('Location: sso.php');
+    header('Location: sso.php', 302);
     exit();
   }
 

b28798c74fed5dafc5da7d8d1fa599fc1c1a5b51
Florent Daigniere <nextgens@freenetproject.org>
2022-11-17 18:46:04 +0100

    doh

--- tests/compose/filters/02_email_antispam.sh
+++ tests/compose/filters/02_email_antispam.sh
@@ -1,6 +1,6 @@
 # GTUBE should be blocked, see https://rspamd.com/doc/gtube_patterns.html
 python3 tests/email_test.py "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X"
-if [ $? -eq 99 ]; then
+if [ $? -eq 25 ]; then
 	exit 0
 else
 	exit 1

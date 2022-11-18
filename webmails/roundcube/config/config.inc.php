e5ab9821f9c7b3d80224222d9928526f5d3743bb
Florent Daigniere <nextgens@freenetproject.org>
2022-11-18 13:25:02 +0100

    Add snuffleupagus
    This seems to work in my limited testing.

--- webmails/roundcube/config/config.inc.php
+++ webmails/roundcube/config/config.inc.php
@@ -5,7 +5,7 @@ $config = array();
 // Generals
 $config['db_dsnw'] = '{{ DB_DSNW }}';
 $config['temp_dir'] = '/dev/shm/';
-$config['des_key'] = '{{ SECRET_KEY }}';
+$config['des_key'] = '{{ ROUNDCUBE_KEY }}';
 $config['cipher_method'] = 'AES-256-CBC';
 $config['identities_level'] = 0;
 $config['reply_all_mode'] = 1;

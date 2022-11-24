32d44b96c37bd38b36fcfd91b0e27251e10d0bed
Florent Daigniere <nextgens@freenetproject.org>
2022-11-24 11:22:43 +0100

    Fix the logic

--- tests/compose/filters/03_email_macro.sh
+++ tests/compose/filters/03_email_macro.sh
@@ -1,14 +1,12 @@
 # Malicious macros should be blocked
 # see https://github.com/clr2of8/VBAstomp and https://github.com/decalage2/oletools/wiki/mraptor
 python3 tests/email_test.py message-macro-stomp "tests/compose/filters/2003x32_word_msgbox_stomped_fakecode.doc"
-if [ $? -eq 25 ]; then
-	exit 0
-else
+if [ $? -ne 25 ]; then
 	exit 1
 fi
 python3 tests/email_test.py message-autoexec-macro "tests/compose/filters/excel4_sample_macro.slk"
-if [ $? -eq 25 ]; then
-	exit 0
-else
+if [ $? -ne 25 ]; then
 	exit 1
 fi
+
+exit 0

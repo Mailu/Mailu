b630355d03faef808a1063bd45f196a949e01819
Florent Daigniere <nextgens@freenetproject.org>
2022-12-08 15:17:58 +0100

    Autofocus the login form on /sso/login

--- core/admin/mailu/sso/forms.py
+++ core/admin/mailu/sso/forms.py
@@ -5,7 +5,7 @@ import flask_wtf
 class LoginForm(flask_wtf.FlaskForm):
     class Meta:
         csrf = False
-    email = fields.StringField(_('E-mail'), [validators.Email(), validators.DataRequired()])
+    email = fields.StringField(_('E-mail'), [validators.Email(), validators.DataRequired()], render_kw={'autofocus': True})
     pw = fields.PasswordField(_('Password'), [validators.DataRequired()])
     pwned = fields.HiddenField(label='', default=-1)
     submitWebmail = fields.SubmitField(_('Sign in'))

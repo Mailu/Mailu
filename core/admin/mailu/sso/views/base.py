03c46aa128632ae29509c534b2a3bd7c5446e590
fastlorenzo <git@bernardi.be>
2022-12-09 00:07:09 +0100

    Moved oic login to /login/oic
    Signed-off-by: fastlorenzo <git@bernardi.be>

--- core/admin/mailu/sso/views/base.py
+++ core/admin/mailu/sso/views/base.py
@@ -16,26 +16,6 @@ def login():
     device_cookie, device_cookie_username = utils.limiter.parse_device_cookie(flask.request.cookies.get('rate_limit'))
     client_ip = flask.request.headers.get('X-Real-IP', flask.request.remote_addr)
 
-    if 'code' in flask.request.args:
-        username, token_response = utils.oic_client.exchange_code(flask.request.query_string.decode())
-        if username is not None:
-            user = models.User.get(username)
-            if user is None: # It is possible that the user never logged into Mailu with his OpenID account
-                user = models.User.create(username) # Create user with no password to enable OpenID-only authentication
-
-            client_ip = flask.request.headers.get('X-Real-IP', flask.request.remote_addr)
-            flask.session["openid_token"] = token_response
-            flask.session.regenerate()
-            flask_login.login_user(user)
-            response = redirect(app.config['WEB_ADMIN'])
-            response.set_cookie('rate_limit', utils.limiter.device_cookie(username), max_age=31536000, path=flask.url_for('sso.login'), secure=app.config['SESSION_COOKIE_SECURE'], httponly=True)
-            flask.current_app.logger.info(f'Login succeeded for {username} from {client_ip}.')
-            return response
-        else:
-            utils.limiter.rate_limit_user(username, client_ip, device_cookie, device_cookie_username) if models.User.get(username) else utils.limiter.rate_limit_ip(client_ip)
-            flask.current_app.logger.warn(f'Login failed for {username} from {client_ip}.')
-            flask.flash('Wrong e-mail or password', 'error')
-            
     form = forms.LoginForm()
     form.submitAdmin.label.text = form.submitAdmin.label.text + ' Admin'
     form.submitWebmail.label.text = form.submitWebmail.label.text + ' Webmail'
@@ -56,10 +36,10 @@ def login():
         username = form.email.data
         if username != device_cookie_username and utils.limiter.should_rate_limit_ip(client_ip):
             flask.flash('Too many attempts from your IP (rate-limit)', 'error')
-            return flask.render_template('login.html', form=form, fields=fields)
+            return flask.render_template('login.html', form=form, fields=fields, openId=app.config['OIDC_ENABLED'], openIdEndpoint=utils.oic_client.get_redirect_url())
         if utils.limiter.should_rate_limit_user(username, client_ip, device_cookie, device_cookie_username):
             flask.flash('Too many attempts for this user (rate-limit)', 'error')
-            return flask.render_template('login.html', form=form, fields=fields)
+            return flask.render_template('login.html', form=form, fields=fields, openId=app.config['OIDC_ENABLED'], openIdEndpoint=utils.oic_client.get_redirect_url())
         user = models.User.login(username, form.pw.data)
         if user:
             flask.session.regenerate()
@@ -76,20 +56,58 @@ def login():
             flask.flash('Wrong e-mail or password', 'error')
     return flask.render_template('login.html', form=form, fields=fields, openId=app.config['OIDC_ENABLED'], openIdEndpoint=utils.oic_client.get_redirect_url())
 
+@sso.route('/login/oic', methods=['GET', 'POST'])
+def login_oic():
+    # Redirect to /login if OIDC is disabled
+    if not app.config['OIDC_ENABLED']:
+        return redirect('/login')
+
+    device_cookie, device_cookie_username = utils.limiter.parse_device_cookie(flask.request.cookies.get('rate_limit'))
+    client_ip = flask.request.headers.get('X-Real-IP', flask.request.remote_addr)
+
+    if 'code' in flask.request.args:
+        username, token_response = utils.oic_client.exchange_code(flask.request.query_string.decode())
+        if username != device_cookie_username and utils.limiter.should_rate_limit_ip(client_ip):
+            flask.flash('Too many attempts from your IP (rate-limit)', 'error')
+            return redirect('/login')
+        if utils.limiter.should_rate_limit_user(username, client_ip, device_cookie, device_cookie_username):
+            flask.flash('Too many attempts for this user (rate-limit)', 'error')
+            return redirect('/login')
+        if username is not None:
+            user = models.User.get(username)
+            # If the user does not exist, create it with an empty password
+            if user is None:
+                user = models.User.create(username)
+
+            flask.session["openid_token"] = token_response
+            flask.session.regenerate()
+            flask_login.login_user(user)
+            # Redirect to the admin interface by default
+            response = redirect(app.config['WEB_ADMIN'])
+            response.set_cookie('rate_limit', utils.limiter.device_cookie(username), max_age=31536000, path=flask.url_for('sso.login'), secure=app.config['SESSION_COOKIE_SECURE'], httponly=True)
+            flask.current_app.logger.info(f'Login succeeded for {username} from {client_ip}.')
+            return response
+        else:
+            utils.limiter.rate_limit_user(username, client_ip, device_cookie, device_cookie_username) if models.User.get(username) else utils.limiter.rate_limit_ip(client_ip)
+            flask.current_app.logger.warn(f'OIDC login failed for {username} from {client_ip}: exchanged code didn\'t return any username.')
+
+    # No code was provided, redirect to the OIDC provider
+    return redirect(utils.oic_client.get_redirect_url())
+
 @sso.route('/logout', methods=['GET'])
 @access.authenticated
 def logout():
     if utils.oic_client.is_enabled():
         if 'openid_token' not in flask.session:
-            return logout_legacy()
+            return logout_internal()
         if 'state' in flask.request.args and 'state' in flask.session:
             if flask.args.get('state') == flask.session['state']:
-                logout_legacy()
+                logout_internal()
         return redirect(utils.oic_client.logout())
-    return logout_legacy()
+    return logout_internal()
     
 
-def logout_legacy():
+def logout_internal():
     flask_login.logout_user()
     flask.session.destroy()
     return flask.redirect(flask.url_for('.login'))

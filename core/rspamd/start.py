0c7ca276cb97df54735806f9cd85386b028c7664
Florent Daigniere <nextgens@freenetproject.org>
2023-02-26 12:17:26 +0100

    remove busy loops

--- core/rspamd/start.py
+++ core/rspamd/start.py
@@ -16,17 +16,6 @@ system.set_env()
 for rspamd_file in glob.glob("/conf/*"):
     conf.jinja(rspamd_file, os.environ, os.path.join("/etc/rspamd/local.d", os.path.basename(rspamd_file)))
 
-# Admin may not be up just yet
-healthcheck = f'http://{os.environ["ADMIN_ADDRESS"]}/internal/rspamd/local_domains'
-while True:
-    time.sleep(1)
-    try:
-        if requests.get(healthcheck,timeout=2).ok:
-            break
-    except:
-        pass
-    log.warning("Admin is not up just yet, retrying in 1 second")
-
 # Run rspamd
 os.system("mkdir -m 755 -p /run/rspamd")
 os.system("chown rspamd:rspamd /run/rspamd")

21982478fb800a659cad130e378a2a3244132179
Florent Daigniere <nextgens@freenetproject.org>
2023-04-20 18:01:16 +0200

    warning is enough

--- core/base/libs/socrate/socrate/system.py
+++ core/base/libs/socrate/socrate/system.py
@@ -80,7 +80,7 @@ def set_env(required_secrets=[], log_filters=[], log_file=None):
     log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", 'WARNING'))
 
     if 'LD_PRELOAD' in os.environ and not _is_compatible_with_hardened_malloc():
-        log.error('Disabling hardened-malloc on this CPU')
+        log.warning('Disabling hardened-malloc on this CPU')
         del os.environ['LD_PRELOAD']
 
     """ This will set all the environment variables and retains only the secrets we need """

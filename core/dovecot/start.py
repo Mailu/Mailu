9284227071b2f7f8670f94baefe23fb5beef8889
Florent Daigniere <nextgens@freenetproject.org>
2023-04-21 17:27:59 +0200

    dovecot is creating zombies
    (cherry picked from commit 36fcb9b830f8fae3e7ccbc48da4cd33cb581ed15)

--- core/dovecot/start.py
+++ core/dovecot/start.py
@@ -9,7 +9,7 @@ import sys
 from podop import run_server
 from socrate import system, conf
 
-system.set_env(log_filters=r'waitpid\(\) returned unknown PID \d+$')
+system.set_env()
 
 def start_podop():
     system.drop_privs_to('mail')

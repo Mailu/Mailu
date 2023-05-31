e195e30a999391715b5029bcfbb4231b453af866
Dimitri Huisman <diman@huisman.xyz>
2023-05-31 09:17:59 +0000

    Update 05_connectivity test to use UTF8 password.
    (cherry picked from commit 9ad7cea5159abda579fb351da7348d09b39dce0c)
    
    # Conflicts:
    #	tests/compose/core/05_connectivity.py

--- tests/compose/core/05_connectivity.py
+++ tests/compose/core/05_connectivity.py
@@ -6,25 +6,31 @@ import smtplib
 import os
 
 SERVER='localhost'
-USERNAME='user@mailu.io'
-PASSWORD='password'
+USERNAME='user_UTF8@mailu.io'
+PASSWORD='password€'
+#https://github.com/python/cpython/issues/73936
+#SMTPlib does not support UTF8 passwords.
+USERNAME_ASCII='user@mailu.io'
+PASSWORD_ASCII='password'
+
 
 def test_imap(server, username, password):
+    auth = lambda data : f'\x00{username}\x00{password}'
     print(f'Authenticating to imaps://{username}:{password}@{server}:993/')
     with imaplib.IMAP4_SSL(server) as conn:
-        conn.login(username, password)
+        conn.authenticate('PLAIN', auth)
         conn.noop()
     print('OK')
     print(f'Authenticating to imaps://{username}:{password}@{server}:143/')
     with imaplib.IMAP4(server) as conn:
         conn.starttls()
-        conn.login(username, password)
+        conn.authenticate('PLAIN', auth)
         conn.noop()
     print('OK')
     print(f'Authenticating to imap://{username}:{password}@{server}:143/')
     try:
         with imaplib.IMAP4(server) as conn:
-            conn.login(username, password)
+            conn.authenticate('PLAIN', auth)
         print(f'Authenticating to imap://{username}:{password}@{server}:143/ worked without STARTTLS!')
         os.exit(102)
     except imaplib.IMAP4.error:
@@ -102,7 +108,41 @@ def test_SMTP(server, username, password):
     except smtplib.SMTPNotSupportedError:
         print('NOK - expected')
 
+<<<<<<< HEAD
 if __name__ == '__main__':
     test_imap(SERVER, USERNAME, PASSWORD)
     test_pop3(SERVER, USERNAME, PASSWORD)
     test_SMTP(SERVER, USERNAME, PASSWORD)
+=======
+def test_managesieve(server, username, password):
+    print(f'Authenticating to sieve://{username}:{password}@{server}:4190/')
+    m=managesieve.MANAGESIEVE(server)
+    try:
+        m.login('PLAIN', username, password)
+        print(f'Worked without STARTTLS!')
+        sys.exit(107)
+    except managesieve.MANAGESIEVE.abort:
+        pass
+
+    m=managesieve.MANAGESIEVE(server, use_tls=True)
+    if m.login('', username, 'wrongpass') != 'NO':
+        print(f'Authenticating to sieve://{username}:{password}@{server}:4190/ with wrong creds has worked!')
+        sys.exit(108)
+
+    if m.login('', username, password) != 'OK':
+        print(f'Authenticating to sieve://{username}:{password}@{server}:4190/ has failed!')
+        sys.exit(109)
+
+    if m.listscripts()[0] != 'OK':
+        print(f'Listing scripts failed!')
+        sys.exit(110)
+    print('OK')
+
+if __name__ == '__main__':
+    test_imap(SERVER, USERNAME, PASSWORD)
+    test_pop3(SERVER, USERNAME, PASSWORD)
+    test_SMTP(SERVER, USERNAME_ASCII, PASSWORD_ASCII)
+    test_managesieve(SERVER, USERNAME, PASSWORD)
+#https://github.com/python/cpython/issues/73936
+#SMTPlib does not support UTF8 passwords.
+>>>>>>> 9ad7cea5 (Update 05_connectivity test to use UTF8 password.)

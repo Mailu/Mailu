e1e765bc00fa92131b18295d6129eded071833a8
Florent Daigniere <nextgens@freenetproject.org>
2023-06-25 18:40:16 +0200

    Add a clue

--- optional/unbound/start.py
+++ optional/unbound/start.py
@@ -10,3 +10,4 @@ system.set_env()
 conf.jinja("/unbound.conf", os.environ, "/etc/unbound/unbound.conf")
 
 os.execv("/usr/sbin/unbound", ["unbound", "-c", "/etc/unbound/unbound.conf"])
+print('Unbound has terminated. If the container keeps restarting, check the internet connectivity of other containers using for instance "docker compose exec front ping example.com"')

28b5df0fa80d6430635e35d0b4c61ee13fe962df
Dimitri Huisman <diman@huisman.xyz>
2023-10-06 09:48:50 +0000

    Hardened malloc was not disabled for oletools when an CPU with missing flags is used
    (cherry picked from commit 77c48294015bb87865cfe3720f545e4ae3a69021)
    
    # Conflicts:
    #	core/oletools/start.py

--- core/oletools/start.py
+++ core/oletools/start.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+<<<<<<< HEAD
+=======
+import os
+>>>>>>> 77c48294 (Hardened malloc was not disabled for oletools when an CPU with missing flags is used)
 from socrate import system
 
 system.set_env()
 
+<<<<<<< HEAD
 with open('/app/olefy.py') as olefy:
     exec(olefy.read())
+=======
+os.execl("/app/olefy.py", "olefy")
+>>>>>>> 77c48294 (Hardened malloc was not disabled for oletools when an CPU with missing flags is used)

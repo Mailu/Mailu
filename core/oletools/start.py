8d9b82510855cc6610194398eb1fe6b742497f01
Florent Daigniere <nextgens@freenetproject.org>
2023-10-07 10:03:23 +0200

    Maybe fix olefy
    (cherry picked from commit 9e1bf76a0ce82ba943779f5ea8cbe75bed0604f8)
    
    # Conflicts:
    #	core/oletools/Dockerfile
    #	core/oletools/start.py

--- core/oletools/start.py
+++ core/oletools/start.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import os
 >>>>>>> 77c48294 (Hardened malloc was not disabled for oletools when an CPU with missing flags is used)
+=======
+>>>>>>> 9e1bf76a (Maybe fix olefy)
 from socrate import system
 
 system.set_env()
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 with open('/app/olefy.py') as olefy:
     exec(olefy.read())
 =======
 os.execl("/app/olefy.py", "olefy")
 >>>>>>> 77c48294 (Hardened malloc was not disabled for oletools when an CPU with missing flags is used)
+=======
+with open('/app/olefy.py') as olefy:
+    exec(olefy.read())
+>>>>>>> 9e1bf76a (Maybe fix olefy)

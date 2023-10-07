447158a09577a29d217428ac17fc143d125d8f6c
Florent Daigniere <nextgens@freenetproject.org>
2023-10-07 10:03:23 +0200

    Maybe fix olefy
    (cherry picked from commit 9e1bf76a0ce82ba943779f5ea8cbe75bed0604f8)

--- core/oletools/start.py
+++ core/oletools/start.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import os
 from socrate import system
 
 system.set_env()
 
-os.execl("/app/olefy.py", "olefy")
+with open('/app/olefy.py') as olefy:
+    exec(olefy.read())

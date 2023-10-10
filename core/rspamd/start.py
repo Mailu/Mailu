c4e8842dfdb97ea2f1daf082aaefb7f5f658f8ee
Dimitri Huisman <diman@huisman.xyz>
2023-10-10 08:19:36 +0000

    Forbidden_file_extension.map could not be overridden.
    (cherry picked from commit 16e9d152dd9a4de69d9ba155a1859aee43387cc9)

--- core/rspamd/start.py
+++ core/rspamd/start.py
@@ -16,7 +16,8 @@ env = system.set_env()
 config_files = []
 for rspamd_file in glob.glob("/conf/*"):
     conf.jinja(rspamd_file, env, os.path.join("/etc/rspamd/local.d", os.path.basename(rspamd_file)))
-    config_files.append(os.path.basename(rspamd_file))
+    if rspamd_file != '/conf/forbidden_file_extension.map':
+        config_files.append(os.path.basename(rspamd_file))
 
 for override_file in glob.glob("/overrides/*"):
     if os.path.basename(override_file) not in config_files:

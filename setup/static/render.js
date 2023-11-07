1a556678f11dcc8b92b4493d5a693a6c9e0ed3e4
Dimitri Huisman <diman@huisman.xyz>
2023-11-07 14:14:27 +0000

    Add extra server-side validation of setup form values Remove deprecated database_flavor JavaScript functionality Remove statistics question which has never been implemented
    (cherry picked from commit eb65e5eea0c11b648f5d878320bf5ae101279b63)

--- setup/static/render.js
+++ setup/static/render.js
@@ -10,17 +10,21 @@ $(document).ready(function() {
 	if ($("#webmail").val() == 'none') {
 		$("#webmail_path").hide();
 		$("#webmail_path").val("");
+		$("#webmail_path").prop('required',false);
 	} else {
 		$("#webmail_path").show();
 		$("#webmail_path").val("/webmail");
+		$("#webmail_path").prop('required',true);
 	}
 	$("#webmail").click(function() {
 		if (this.value == 'none') {
 			$("#webmail_path").hide();
 			$("#webmail_path").val("");
+			$("#webmail_path").prop('required',false);
 		} else {
 			$("#webmail_path").show();
 			$("#webmail_path").val("/webmail");
+			$("#webmail_path").prop('required',true);
 		}
 	});
 });
@@ -29,14 +33,17 @@ $(document).ready(function() {
 	if ($('#admin').prop('checked')) {
 		$("#admin_path").show();
 		$("#admin_path").val("/admin");
+		$("#admin_path").prop('required',true);
 	}
 	$("#admin").change(function() {
 		if ($(this).is(":checked")) {
 			$("#admin_path").show();
 			$("#admin_path").val("/admin");
+			$("#admin_path").prop('required',true);
 		} else {
 			$("#admin_path").hide();
 			$("#admin_path").val("");
+			$("#admin_path").prop('required',false);
 		}
 	});
 });
@@ -44,6 +51,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	if ($('#api_enabled').prop('checked')) {
 		$("#api_path").show();
+		$("#api_path").prop('required',true);
 		$("#api_path").val("/api")
 		$("#api_token").show();
 		$("#api_token").prop('required',true);
@@ -51,6 +59,7 @@ $(document).ready(function() {
 		$("#api_token_label").show();
 	} else {
 		$("#api_path").hide();
+		$("#api_path").prop('required',false);
 		$("#api_path").val("")
 		$("#api_token").hide();
 		$("#api_token").prop('required',false);
@@ -60,6 +69,7 @@ $(document).ready(function() {
 	$("#api_enabled").change(function() {
 		if ($(this).is(":checked")) {
 			$("#api_path").show();
+			$("#api_path").prop('required',true);
 			$("#api_path").val("/api");
 			$("#api_token").show();
 			$("#api_token").prop('required',true);
@@ -67,50 +77,12 @@ $(document).ready(function() {
 			$("#api_token_label").show();
 		} else {
 			$("#api_path").hide();
+			$("#api_path").prop('required',false);
 			$("#api_path").val("")
 			$("#api_token").hide();
 			$("#api_token").prop('required',false);
 			$("#api_token").val("");
 			$("#api_token_label").hide();
-		}
-	});
-});
-
-$(document).ready(function() {
-	if ($("#database").val() == 'sqlite') {
-		$("#external_db").hide();
-	} else {
-		$("#external_db").show();
-	}
-
-	$("#webmail").click(function() {
-		if (this.value == 'roundcube') {
-			$("#db_flavor_rc_sel").show();
-		} else {
-			$("#db_flavor_rc_sel").hide();
-			$("#roundcube_db_user,#roundcube_db_pw,#roundcube_db_url,#roundcube_db_name").prop('required',false);
-		}
-	});
-
-	$("#database").click(function() {
-		if (this.value == 'sqlite') {
-			$("#external_db").hide();
-			$("#db_user,#db_pw,#db_url,#db_name").prop('required',false);
-			$("#roundcube_db_user,#roundcube_db_pw,#roundcube_db_url,#roundcube_db_name").prop('required',false);
-		} else {
-			$("#external_db").show();
-			$("#db_user,#db_pw,#db_url,#db_name").prop('required',true);
-		}
-	});
-
-	$("#database_rc").click(function() {
-		if (this.value == 'sqlite'){
-			$("#roundcube_external_db").hide();
-			$("#roundcube_db_user,#roundcube_db_pw,#roundcube_db_url,#roundcube_db_name").prop('required',false);
-		}
-		else if ($("#webmail").val() == 'roundcube') {
-			$("#roundcube_external_db").show();
-			$("#roundcube_db_user,#roundcube_db_pw,#roundcube_db_url,#roundcube_db_name").prop('required',true);
 		}
 	});
 });

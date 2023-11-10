5c9e20a42ae7fc52da639536a6b6cddc0da74046
Florent Daigniere <nextgens@freenetproject.org>
2023-11-10 12:25:34 +0100

    doh
    (cherry picked from commit 2c422bbfbede5a8967d55102e8e1525ffd4a6e2b)

--- tests/compose/filters/01_email_test.sh
+++ tests/compose/filters/01_email_test.sh
@@ -2,7 +2,7 @@ python3 tests/email_test.py message-virus "tests/compose/filters/eicar.com.txt"
 if [ $? -eq 99 ]; then
     python3 tests/email_test.py message-PUA "tests/compose/filters/PotentiallyUnwanted.exe_"
     if [ $? -eq 99 ]; then
-        return 0
+        exit 0
     else
         exit 1
     fi

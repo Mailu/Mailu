ecaa6f37e3bc6641748dac14abcec9b25bb0e863
Florent Daigniere <nextgens@freenetproject.org>
2023-11-10 12:13:04 +0100

    Add a new test for PUAs
    (cherry picked from commit f7fb0f66256a34c36766fcc24e79ce0912e293c7)

--- tests/compose/filters/01_email_test.sh
+++ tests/compose/filters/01_email_test.sh
@@ -1,6 +1,11 @@
 python3 tests/email_test.py message-virus "tests/compose/filters/eicar.com.txt"
 if [ $? -eq 99 ]; then
-	exit 0
+    python3 tests/email_test.py message-PUA "tests/compose/filters/PotentiallyUnwanted.exe_"
+    if [ $? -eq 99 ]; then
+        return 0
+    else
+        exit 1
+    fi
 else
 	exit 1
 fi

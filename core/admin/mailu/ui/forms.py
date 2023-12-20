30574445e457e4562443cde0dbdcd8146c2fbc1a
Florent Daigniere <nextgens@freenetproject.org>
2023-12-20 11:41:45 +0100

    Better regexp

--- core/admin/mailu/ui/forms.py
+++ core/admin/mailu/ui/forms.py
@@ -38,8 +38,8 @@ class MultipleEmailAddressesVerify(object):
         self.message = message
 
     def __call__(self, form, field):
-        pattern = re.compile(r'^([_a-z0-9\-\+]+)(\.[_a-z0-9\-\+]+)*@([a-z0-9\-]{1,}\.)*([a-z]{1,})(,([_a-z0-9\-\+]+)(\.[_a-z0-9\-\+]+)*@([a-z0-9\-]{1,}\.)*([a-z]{2,}))*$')
-        if not pattern.match(field.data.lower().replace(" ", "")):
+        pattern = re.compile(r"^(?:\s*\S+@([a-z0-9\-]{1,}\.)*([a-z]{1,})[\s|,]*)+$", re.IGNORECASE)
+        if not pattern.match(field.data.replace(" ", "")):
             raise validators.ValidationError(self.message)
 
 class MultipleFoldersVerify(object):

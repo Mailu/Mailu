8b34fbe8810239d36134cd9756904112d932e194
Florent Daigniere <nextgens@freenetproject.org>
2024-09-12 19:21:18 +0200

    Maybe fix #3402

--- core/nginx/letsencrypt.py
+++ core/nginx/letsencrypt.py
@@ -6,8 +6,6 @@ import requests
 import sys
 import subprocess
 import time
-from threading import Thread
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 log.basicConfig(stream=sys.stderr, level="WARNING")
 hostnames = ','.join(set(host.strip() for host in os.environ['HOSTNAMES'].split(',')))
@@ -45,33 +43,20 @@ command2 = [
 # Wait for nginx to start
 time.sleep(5)
 
-class MyRequestHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/.well-known/acme-challenge/testing':
-            self.send_response(204)
-        else:
-            self.send_response(404)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-
-def serve_one_request():
-    with HTTPServer(("127.0.0.1", 8008), MyRequestHandler) as server:
-        server.handle_request()
-
 # Run certbot every day
 while True:
     while True:
         hostname = os.environ['HOSTNAMES'].split(',')[0]
         target = f'http://{hostname}/.well-known/acme-challenge/testing'
-        thread = Thread(target=serve_one_request)
-        thread.start()
-        r = requests.get(target)
-        if r.status_code != 204:
-            log.critical(f"Can't reach {target}!, please ensure it's fixed or change the TLS_FLAVOR.")
-            time.sleep(5)
-        else:
-            break
-        thread.join()
+        try:
+            r = requests.get(target)
+            if r.status_code != 204:
+                log.critical(f"Can't reach {target}!, please ensure it's fixed or change the TLS_FLAVOR.")
+                time.sleep(5)
+            else:
+                break
+        except Exception as e:
+            log.error(f"Exception while fetching {target}!", exc_info = e)
 
     subprocess.call(command)
     subprocess.call(command2)

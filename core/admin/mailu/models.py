ced276303525c2db15678528f6f524f554848778
Jumper78 <52802286+Jumper78@users.noreply.github.com>
2025-02-25 15:32:34 +0100

    simplify function Domain.dns_dmarc_report_needed
    Co-authored-by: Florent Daigniere <nextgens@users.noreply.github.com>

--- core/admin/mailu/models.py
+++ core/admin/mailu/models.py
@@ -249,6 +249,7 @@ class Domain(Base):
 
     @cached_property
     def dns_dmarc_report_needed(self):
+        return self.name != app.config['DOMAIN']
         """ return true if DMARC report record is needed """
         domain = app.config['DOMAIN']
         if self.name != domain:

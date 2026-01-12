c0f8b0eba8a8d0c53231984cc7ae5c0d980575d6
Bonito Bonita <bonitobonita24@gmail.com>
2026-01-12 16:49:26 +0000

    Redirect to inbox after successful password change

--- webmails/roundcube/login/mailu.php
+++ webmails/roundcube/login/mailu.php
@@ -229,6 +229,9 @@ class mailu extends rcube_plugin
       // Update the stored password for current session
       $_SESSION['password'] = $rcmail->encrypt($newpasswd);
       $rcmail->output->command('display_message', $this->gettext('successfullysaved'), 'confirmation');
+      // Redirect to mail inbox after successful password change
+      $rcmail->output->redirect('./?_task=mail');
+      return;
     } else {
       $message = isset($result['message']) ? $result['message'] : $this->gettext('errorsaving');
       $rcmail->output->command('display_message', $message, 'error');
